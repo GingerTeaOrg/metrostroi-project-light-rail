99fd53eecf3a1da6bc9f1246468d93db20d787f7
Lilly Evans <5427724+LillyWho@users.noreply.github.com>
2022-09-27 20:31:00 +0200

    Why use a controller when the Display can do everything

--- lua/entities/gmod_uf_dfi_controller/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-
-ENT.PrintName		= "Lumina DFO Controller"
-ENT.Author			= "Ron"
-ENT.Category 		= "Metrostroi Frankfurt"
-ENT.Editable 		= false
-ENT.Spawnable 		= false
-ENT.AdminOnly 		= false
