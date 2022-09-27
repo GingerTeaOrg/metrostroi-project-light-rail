efd1689cb1a0da4f8eefe08948c76aa65e247b5e
Lilly Evans <5427724+LillyWho@users.noreply.github.com>
2022-09-27 21:47:58 +0200

    Prepare refactor for Frankfurt Lumino

--- lua/entities/gmod_uf_dfi_display/cl_init.lua
+++ lua/entities/gmod_uf_dfi_display/cl_init.lua
@@ -5,12 +5,18 @@ function ENT:Initialize()
 	self.Minute = self.Minute or 0
 	self.Hour = self.Hour or 0
 	self.AttachmentTable = {}
-	local AttachmentNames = {"Abfahrt01A", "Abfahrt01B", 
+	local AttachmentNames = {				 "Abfahrt01A", "Abfahrt01B", 
 								 "Abfahrt02A", "Abfahrt02B",
+								 "Abfahrt03A", "Abfahrt03B",
+								 "Abfahrt04A", "Abfahrt04B",
 								 "Ziel01A", "Ziel01B",
 								 "Ziel02A", "Ziel02B",
+								 "Ziel03A", "Ziel03B",
+								 "Ziel04A", "Ziel04A",
 								 "Linie01A", "Linie01B",
 								 "Linie02A", "Linie02B",
+								 "Linie03A", "Linie03B",
+								 "Linie04A", "Linie04B",
 								 "GleisA", "GleisB",
 								 "UhrA", "UhrB"}
 	for i=1,table.Count( AttachmentNames ) do 
@@ -36,40 +42,41 @@ function ENT:SpawnZeiger()
 	self.zeiger_grossB = nil
 	self.zeiger_kleinB = nil
 		
-	self.zeiger_grossA = ents.CreateClientProp("models/props_borealis/mooring_cleat01.mdl") 
-	self.zeiger_grossA:SetModel("models/ron/gm_metro_u6/station/uhrzeiger_gross.mdl")
+	self.zeiger_grossA = ents.CreateClientProp("/models/lilly/uf/trackside/lumino/minute.mdl") 
+	self.zeiger_grossA:SetModel("models/lilly/uf/trackside/lumino/minute.mdl")
 	self.zeiger_grossA:PhysicsInit( SOLID_VPHYSICS )
 	self.zeiger_grossA:SetMoveType( MOVETYPE_NONE  )
 	self.zeiger_grossA:SetSolid( SOLID_NONE )
-	self.zeiger_grossA:SetPos(self.AttachmentTable[15].Pos)
-	self.zeiger_grossA:SetAngles(self.AttachmentTable[15].Ang)
+	self.zeiger_grossA:SetPos(self.AttachmentTable[27].Pos)
+	self.zeiger_grossA:SetAngles(self.AttachmentTable[27].Ang)
 	self.zeiger_grossA:SetParent(self)
 
 	self.zeiger_kleinA = ents.CreateClientProp("models/props_borealis/mooring_cleat01.mdl")
-	self.zeiger_kleinA:SetModel("models/ron/gm_metro_u6/station/uhrzeiger_klein.mdl")
+	self.zeiger_kleinA:SetModel("models/lilly/uf/trackside/lumino/hour.mdl")
 	self.zeiger_kleinA:PhysicsInit( SOLID_VPHYSICS ) 
 	self.zeiger_kleinA:SetMoveType( MOVETYPE_NONE  )
 	self.zeiger_kleinA:SetSolid( SOLID_NONE )
-	self.zeiger_kleinA:SetPos(self.AttachmentTable[15].Pos)
-	self.zeiger_kleinA:SetAngles(self.AttachmentTable[15].Ang)
+	self.zeiger_kleinA:SetPos(self.AttachmentTable[27].Pos)
+	self.zeiger_kleinA:SetAngles(self.AttachmentTable[27].Ang)
 	self.zeiger_kleinA:SetParent(self)
 
-	self.zeiger_grossB = ents.CreateClientProp("models/props_borealis/mooring_cleat01.mdl")
-	self.zeiger_grossB:SetModel("models/ron/gm_metro_u6/station/uhrzeiger_gross.mdl")
+		
+	self.zeiger_grossA = ents.CreateClientProp("/models/lilly/uf/trackside/lumino/minute.mdl") 
+	self.zeiger_grossA:SetModel("models/lilly/uf/trackside/lumino/minute.mdl")
 	self.zeiger_grossB:PhysicsInit( SOLID_VPHYSICS )
 	self.zeiger_grossB:SetMoveType( MOVETYPE_NONE  )
 	self.zeiger_grossB:SetSolid( SOLID_NONE )
-	self.zeiger_grossB:SetPos(self.AttachmentTable[16].Pos)
-	self.zeiger_grossB:SetAngles(self.AttachmentTable[16].Ang)
+	self.zeiger_grossB:SetPos(self.AttachmentTable[28].Pos)
+	self.zeiger_grossB:SetAngles(self.AttachmentTable[28].Ang)
 	self.zeiger_grossB:SetParent(self)
-	
-	self.zeiger_kleinB = ents.CreateClientProp("models/props_borealis/mooring_cleat01.mdl") 
-	self.zeiger_kleinB:SetModel("models/ron/gm_metro_u6/station/uhrzeiger_klein.mdl")
+
+	self.zeiger_kleinA = ents.CreateClientProp("models/props_borealis/mooring_cleat01.mdl")
+	self.zeiger_kleinA:SetModel("models/lilly/uf/trackside/lumino/hour.mdl")
 	self.zeiger_kleinB:PhysicsInit( SOLID_VPHYSICS )
 	self.zeiger_kleinB:SetMoveType( MOVETYPE_NONE  )
 	self.zeiger_kleinB:SetSolid( SOLID_NONE )
-	self.zeiger_kleinB:SetPos(self.AttachmentTable[16].Pos)
-	self.zeiger_kleinB:SetAngles(self.AttachmentTable[16].Ang)
+	self.zeiger_kleinB:SetPos(self.AttachmentTable[28].Pos)
+	self.zeiger_kleinB:SetAngles(self.AttachmentTable[28].Ang)
 	self.zeiger_kleinB:SetParent(self)
 	
 	self.zeiger_gross = {self.zeiger_grossA, self.zeiger_grossB}
@@ -81,36 +88,6 @@ end
 function ENT:Draw()
 	self:DrawModel()
 
-	self.TrackNumber = self:GetNWInt("TrackNumber", 0)
-	self.TextDepartA = self:GetNWString("TextDepartA", 0)
-	self.TextDepartB = self:GetNWString("TextDepartB", 0)
-	self.TextDestA = self:GetNWString("TextDestA", 0)
-	self.TextDestB = self:GetNWString("TextDestB", 0)
-	self.TextLineA = self:GetNWString("TextLineA", 0)
-	self.TextLineB = self:GetNWString("TextLineB", 0)
-	
-	if !self.AttachmentTable then return end
-	-- Draw Departure Time
-	self:drawText(self.AttachmentTable, 1,  0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDepartA) 
-	self:drawText(self.AttachmentTable, 2,  0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDepartA)
-	self:drawText(self.AttachmentTable, 3,  0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDepartB)
-	self:drawText(self.AttachmentTable, 4,  0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDepartB)
-	
-	-- Draw Destinations
-	self:drawText(self.AttachmentTable, 5,  0.14, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDestA)
-	self:drawText(self.AttachmentTable, 6,  0.14, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDestA)
-	self:drawText(self.AttachmentTable, 7,  0.14, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDestB)
-	self:drawText(self.AttachmentTable, 8,  0.14, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextDestB)
-	
-	-- Draw Lines
-	self:drawText(self.AttachmentTable, 9,  0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextLineA)
-	self:drawText(self.AttachmentTable, 10, 0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextLineA)
-	self:drawText(self.AttachmentTable, 11, 0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextLineB)
-	self:drawText(self.AttachmentTable, 12, 0.15, "DaisyAnzeige", Color(255, 216, 0, 255), self.TextLineB)
-
-	-- Draw Track Number 
-	self:drawText(self.AttachmentTable, 13, 0.1, "GleisAnzeige", Color(255, 255, 255, 255), self.TrackNumber)
-	self:drawText(self.AttachmentTable, 14, 0.1, "GleisAnzeige", Color(255, 255, 255, 255), self.TrackNumber)
 end 
 
 function ENT:drawText(Atttable, index, scale, font, color, text)
