cb376b8c2a37dbd498a6d7db614d2fc5a63ad86d
lillywho <lillywho@outlook.de>
2023-07-30 13:05:23 +0200

    Capitalisation, fss

--- lua/autorun/uf.lua
+++ lua/autorun/uf.lua
@@ -473,5 +473,5 @@ if CLIENT then
         AddCSLuaFile("uf/"..filename)
         include("uf/"..filename)
     end
-    runconsolecommand(metrostroi_drawdistance 99999)
+    RunConsoleCommand(metrostroi_drawdistance 99999)
 end
