db3db2d0e645bd2baa88b282a6874d8fcf2051b2
lillywho <lillywho@outlook.de>
2023-08-25 14:57:08 +0200

    Modify convars

--- lua/uf/sv_convars.lua
+++ lua/uf/sv_convars.lua
@@ -1,7 +1,7 @@
 CreateConVar("uf_current_limit",4000,FCVAR_ARCHIVE)
-CreateConVar("uf_train_requirewire",1,FCVAR_ARCHIVE,"Whether or not Light Rail trains require power from the overhead wire.")
+CreateConVar("uf_train_requirewire",1,FCVAR_NOTIFY,"Whether or not Light Rail trains require power from the overhead wire.")
 
-function Metrostroi.GetEnergyCost(kWh)
+function UF.GetEnergyCost(kWh)
     return kWh*0.08
 end
 
