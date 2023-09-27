4e15b56edc6d4c3fe73cc9300ccfd0d295521dae
lillywho <lillywho@outlook.de>
2023-09-27 10:58:43 +0200

    Finish remote control features of UF switch entity

--- lua/entities/gmod_track_uf_switch/init.lua
+++ lua/entities/gmod_track_uf_switch/init.lua
@@ -5,11 +5,19 @@ function ENT:Initialize()
 	self:SetModel("models/metrostroi/signals/box.mdl")
 	Metrostroi.DropToFloor(self)
 	
-	self.AutomaticRouting = false --radio-controlled by INDUSI/PZB when in tram operation or by dispatcher/manually
+	self.ID = 000
 	-- Initial state of the switch
 	self.AlternateTrack = false
+	self.AlreadyLocked = false
+
 	self.InhibitSwitching = false
+	self.SignalCaller = {}
 	self.LastSignalTime = 0
+
+	self.Left = GetInternalVariable("MetrostroiValueForLeft") or UF.SwitchTable[self.ID["left"]]
+	self.Right = self.Left == "main" and "alt" or self.Left == "alt" and "main"
+
+	self.TrackPosition = Metrostroi.GetPositionOnTrack(self:GetPos())
 
 	-- Find rotating parts which belong to this switch
 	local list = ents.FindInSphere(self:GetPos(),256)
@@ -31,9 +39,18 @@ function ENT:OnRemove()
 	UF.UpdateSignalEntities()
 end
 
-function ENT:SendSignal(index,channel)
-	if channel ~= self:GetChannel() then return end
+function ENT:SendSignal(index,train)
 	
+	for _,v in pairs(self.SignalCaller) do --insert the train that called the command into a table in order to maintain a precedence of sorts
+		if v == train then
+			break
+		else
+			table.insert(self.SignalCaller,train)
+		end
+	end
+
+	if train ~= self.SignalCaller[1] then return false end --first come, first serve. Only get your command heeded when you're top of the list
+
 	-- Switch to alternate track
 	if index == "alt" then self.AlternateTrack = true end
 	-- Switch to main track
@@ -42,6 +59,23 @@ function ENT:SendSignal(index,channel)
 	-- Remember this signal
 	self.LastSignal = index
 	self.LastSignalTime = CurTime()
+	self.LastSignalCaller = self.SignalCaller[1]
+
+end
+
+function ENT:IsTrainInRange(train)
+	local pos = self.TrackPosition --the switch coordinates on the node system
+
+	local SwitchVector = Vector(pos.x,pos.y,pos.z) --Get the x y z coordinates and express them as a vector
+	local Train = Metrostroi.GetPositionOnTrack(self.SignalCaller[1]:GetPos()) --same for the train
+	local TrainVector = Vector(Train.x,Train.y,Train.z)
+
+	if SwitchVector:Distance(TrainVector) <= 10 then
+		return true
+	end
+
+	return false
+
 end
 
 function ENT:GetSignal()
@@ -72,13 +106,17 @@ function ENT:Think()
 	
 	-- Force signal
 	if self.LockedSignal then
-		self:SendSignal(self.LockedSignal,self:GetChannel())
+		self:SendSignal(self.LockedSignal)
 	end
 	
 	-- Return switch to original position
 	if (self.InhibitSwitching == false) and (self.AlternateTrack == true) and
 	   (CurTime() - self.LastSignalTime > 20.0) then
-		self:SendSignal("main",self:GetChannel())
+		self:SendSignal("main")
+	end
+
+	if self:IsTrainInRange(self.LastSignalCaller) == false then
+		table.remove(self.SignalCaller,1) --at the end of it all, remove the train from the list
 	end
 	
 	-- Process logic
