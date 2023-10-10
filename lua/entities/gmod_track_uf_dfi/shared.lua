ccafffd27456015dd2fab228784dc6d67352c48d
lillywho <lillywho@outlook.de>
2023-10-10 10:40:40 +0200

    Frankfurt announcement prep for Lumino

--- lua/entities/gmod_track_uf_dfi/shared.lua
+++ lua/entities/gmod_track_uf_dfi/shared.lua
@@ -17,15 +17,20 @@ ENT.Panel1Angle = {Angle(0, 15, 0)}
 ENT.Panel2 = {Vector(100, 0, 0), Vector(200, -50, 0)}
 ENT.Panel2Angle = {Angle(0, 15, 0)}
 
-function ENT:PlayOnceFromPos(id, sndname, volume, pitch, min, max, location)
+local function destroySound(snd,nogc)
+	if IsValid(snd) then snd:Stop() end
+	if not nogc and snd and snd.__gc then snd:__gc() end
+end
+function ENT:DestroySound(snd,nogc)
+	destroySound(snd,nogc)
+end
+
+function ENT:PlayOnceFromPos(sndname, volume, pitch, min, max, location)
 	if self.StopSounds then return end
-	self:DestroySound(self.Sounds[id], true)
-	self.Sounds[id] = nil
+	self:DestroySound(sndname, true)
 	if sndname == "_STOP" then return end
-	self.SoundPositions[id] = {min, max, location}
 	self:CreateBASSSound(sndname, function(snd)
-		self.Sounds[id] = snd
-		self:SetBassParameters(self.Sounds[id], pitch, volume, self.SoundPositions[id], false)
+		self:SetBassParameters(sndname, pitch, volume, location, false)
 		snd:Play()
 	end)
 end
@@ -57,3 +62,37 @@ function ENT:CreateBASSSound(name, callback, noblock, onerr)
 		end
 	end)
 end
+function ENT:SetBASSPos(snd,tbl)
+	if tbl then
+		snd:SetPos(self:LocalToWorld(tbl[3]),self:GetAngles():Forward())
+	else
+		snd:SetPos(self:GetPos())
+	end
+end
+
+function ENT:SetBassParameters(snd,pitch,volume,tbl,looping,spec)
+	if snd:GetState() ~= GMOD_CHANNEL_STOPPED and snd:GetState() ~= GMOD_CHANNEL_PAUSED then
+		return
+	end
+	self:SetBASSPos(snd,tbl)
+	if tbl then
+		snd:Set3DFadeDistance(tbl[1],tbl[2])
+		if tbl[4] then
+			snd:SetVolume(tbl[4]*volume)
+		else
+			snd:SetVolume(volume)
+		end
+	else
+		snd:Set3DFadeDistance(200,1e9)
+		snd:SetVolume(volume)
+	end
+	snd:EnableLooping(looping or false)
+	snd:SetPlaybackRate(pitch)
+	local siz1,siz2 = snd:Get3DFadeDistance()--[[]
+	debugoverlay.Sphere(snd:GetPos(),4,2,Color(0,255,0),true)
+	debugoverlay.Sphere(snd:GetPos(),siz1,2,Color(255,0,0,100),false)]]
+	--debugoverlay.Sphere(snd:GetPos(),siz2,2,Color(0,0,255,100),false)
+end
+
+
+ENT.AnnouncementSounds = {"U1 Richtung Ginnheim", "U1 Richtung Heddernheim","U1 Richtung Roemerstadt", "U1 Richtung Suedbahnhof", "U2 Richtung Gonzenheim", "U2 Richtung Heddernheim", "U2 Richtung Nieder-Eschbach", "U2 Richtung Suedbahnhof", "U3 Richtung Heddernheim", "U3 Richtung Hohemark", "U3 Richtung Oberursel Bahnhof", "U3 Richtung Oberursel Bommersheim", "U3 Richtung Suedbahnhof", "U4 Richtung Bockenheimer Warte", "U4 Richtung Enkheim", "U4 Richtung Hauptbahnhof", "U4 Richtung Konstablerwache", "U4 Richtung Schaefflestrasse", "U4 Richtung Seckbacher Ldstr", "U5 Richtung Eckenh Ldstr Marbachweg", "U5 Richtung Hauptbahnhof", "U5 Richtung Hauptfriedhof", "U5 Richtung Konstablerwache", "U5 Richtung Preungesheim", "U6 Richtung Bockenheimer Warte", "U6 Richtung Heerstrasse", "U6 Johanna-Tesch-Platz", "U6 Richtung Ostbahnhof", "U6 Richtung Zoo", "U7 Richtung Enkheim", "U7 Richtung Hausen", "U7 Richtung Johanna-Tesch-Platz", "U7 Richtung Schaefflestrasse", "U7 Richtung Zoo", "U8 Richtung Heddernheim", "U8 Richtung Riedberg", "U8 Richtung Suedbahnhof", "U8 Richtung Ginnheim", "U8 Richtung Nieder-Eschbach", "U9 Richtung Nieder-Eschbach", "U9 Richtung Roemerstadt"}
