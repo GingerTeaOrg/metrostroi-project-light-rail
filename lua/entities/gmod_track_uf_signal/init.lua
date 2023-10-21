928b1d35864caaa8df7de7a47b9d8850f02b7bea
lillywho <lillywho@outlook.de>
2023-10-21 16:55:40 +0200

    Further work on Signal

--- lua/entities/gmod_track_uf_signal/init.lua
+++ lua/entities/gmod_track_uf_signal/init.lua
@@ -11,161 +11,27 @@ function ENT:Initialize()
 	self.Sprites = {}
 	self:SetModel("models/lilly/uf/signals/Underground_Small_Pole.mdl" or self.SignalType)
 	self.Name1 = "Tbf"
-	self.Name2 = "B2"
+	self.Name2 = "B1"
 	self.Name = self.Name1.."/"..self.Name2
 	self:SetPos(self:GetPos() - Vector(0,0,15))
 	self.TrackPosition = Metrostroi.GetPositionOnTrack(self:GetPos(),self:GetAngles())[1]
+	self.Node = self.TrackPosition.node1
 
+	self.BlockSegment = Metrostroi.GetARSJoint(self.TrackPosition.node1,self.TrackPosition.x,self.TrackPosition.forward,false)
 	self.Aspect = "H0"
 	self.SpeedLimit = 0
 
 	self.Left = false
-end
-
-function ENT:OpenRoute(route)
-	self.LastOpenedRoute = route
-	if self.Routes[route].Manual then self.Routes[route].IsOpened = true end
-	if not self.Routes[route].Switches then return end
-	local Switches = string.Explode(",",self.Routes[route].Switches)
-
-	for i1 =1, #Switches do
-		if not Switches[i1] or Switches[i1] == "" then continue end
-
-		local SwitchState = Switches[i1]:sub(-1,-1) == "-"
-		local SwitchName = Switches[i1]:sub(1,-2)
-		--if not self.Switches[SwitchName] then self.Switches[SwitchName] = Metrostroi.GetSwitchByName(SwitchName) end
-		if not Metrostroi.GetSwitchByName(SwitchName) then print(self.Name,"switch not found") continue end
-		--If route go right from this switch - add it
-		if SwitchState ~= (Metrostroi.GetSwitchByName(SwitchName):GetSignal() ~= 0) then
-			Metrostroi.GetSwitchByName(SwitchName):SendSignal(SwitchState and "alt" or "main",nil,true)
-			--RunConsoleCommand("say","changing",SwitchName)
-		end
-	end
-end
-
-function ENT:CloseRoute(route)
-	if self.Routes[route].Manual then self.Routes[route].IsOpened = false end
-	if not self.Routes[route].Switches then return end
-
-	local Switches = string.Explode(",",self.Routes[route].Switches)
-	for i1 =1, #Switches do
-		if not Switches[i1] or Switches[i1] == "" then continue end
-
-		--local SwitchState = Switches[i1]:sub(-1,-1) == "-"
-		local SwitchName = Switches[i1]:sub(1,-2)
-		--if not self.Switches[SwitchName] then self.Switches[SwitchName] = Metrostroi.GetSwitchByName(SwitchName) end
-		if not Metrostroi.GetSwitchByName(SwitchName) then print(self.Name,"switch not found") continue end
-		--If route go right from this switch - add it
-		if SwitchState ~= (Metrostroi.GetSwitchByName(SwitchName):GetSignal() ~= 0) then
-			Metrostroi.GetSwitchByName(SwitchName):SendSignal("main",nil,true)
-			--RunConsoleCommand("say","changing",SwitchName)
-		end
-	end
+	UF.UpdateSignalEntities()
 end
 
 
 
 
-function ENT:PreInitalize()
-
-
-	self.NextSignals = {}
-	--self.Switches = {}
-	for k,v in ipairs(self.Routes) do
-		if v.NextSignal == "" then
-			self.NextSignals[""] = nil--self
-		elseif v.NextSignal == "*" then
-		else
-			if not v.NextSignal then
-				ErrorNoHalt(Format("MPLR: No next signal name in signal %s! Check it now!\n", self.Name))
-			else
-				self.NextSignals[v.NextSignal] = Metrostroi.GetSignalByName(v.NextSignal)
-				if not self.NextSignals[v.NextSignal] then
-					print(Format("MPLR: Signal %s, signal not found(%s)", self.Name, v.NextSignal))
-				end
-			end
-		end
-	end
-end
-function ENT:PostInitalize()
-	if not self.Routes or #self.Routes == 0 then print(self, "NEEDS SETUP") return end
-	for k,v in ipairs(self.Routes) do
-		if v.NextSignal == "*" and self.TrackPosition then
-			local sig
-			local cursig = self
-			while true do
-				cursig = Metrostroi.GetARSJoint(cursig.TrackPosition.node1,cursig.TrackPosition.x,cursig.TrackDir,false)
-				if not IsValid(cursig) then break end
-				sig = cursig
-				if not cursig.PassOcc then break end
-			end
-			if IsValid(sig) then
-				self.NextSignals["*"] = sig
-			else
-				self.AutostopOverride = true
-				print(Format("MPLR: Signal %s, cant automaticly find signal", self.Name))
-			end
-		end
-	end
-	local pos = self.TrackPosition
-	local node = pos and pos.node1 or nil
-	self.Node = node
-
-	self.SwitchesFunction = {}
-	self.Switches = {}
-	for i = 1,#self.Routes do
-		if not self.Routes[i].Switches then continue end
-
-		local Switches = string.Explode(",",self.Routes[i].Switches)
-		local SwitchesTbl = {}
-		--local GoodSwitches = true
-		--Checking all route switches
-		for i1 =1, #Switches do
-			if not Switches[i1] or Switches[i1] == "" then continue end
-
-			local SwitchState = Switches[i1]:sub(-1,-1) == "-"
-			local SwitchName = Switches[i1]:sub(1,-2)
-			if not Metrostroi.GetSwitchByName(SwitchName) then
-				print(Format("MPLR: %s, switch not found(%s)", self.Name, SwitchName))
-				continue
-			end
-			--If route go right from this switch - add it
-			table.insert(SwitchesTbl,{n = SwitchName,s = SwitchState})
-		end
-		self.Switches[i] = SwitchesTbl
-		if #SwitchesTbl == 0 then continue end
-		self.SwitchesFunction[i] = function()
-			local GoodSwitches = true
-			for i1 = 1,#self.Switches[i] do
-				if not self.Switches[i][i1] or not IsValid(Metrostroi.GetSwitchByName(self.Switches[i][i1].n)) then continue end
-				if self.Switches[i][i1].s ~= (Metrostroi.GetSwitchByName(self.Switches[i][i1].n):GetSignal() > 0) then
-					GoodSwitches = false
-					break
-				end
-			end
-			return GoodSwitches
-		end
-	end
-	for k,v in pairs(self.Routes) do
-		if not v.Lights then continue end
-		v.LightsExploded = string.Explode("-",v.Lights)
-	end
-
-
-	self.PostInitalized = false
-
-end
 
 function ENT:OnRemove()
 	Metrostroi.UpdateSignalEntities()
 	Metrostroi.PostSignalInitialize()
-end
-
-function ENT:GetARS(ARSID,Force1_5,Force2_6)
-	if self.OverrideTrackOccupied then return ARSID == 2 end
-	if not self.ARSSpeedLimit then return false end
-	local nxt = self.ARSNextSpeedLimit == 2 and 0 or self.ARSNextSpeedLimit ~= 1 and self.ARSNextSpeedLimit
-	return self.ARSSpeedLimit == ARSID or ((self.TwoToSix and not Force1_5 or Force2_6) and nxt and nxt == ARSID and self.ARSSpeedLimit > nxt)
 end
 
 
@@ -177,15 +43,15 @@ function ENT:GetRS()
 	return (self.ARSSpeedLimit > 4 or self.ARSSpeedLimit == 4 and self.Approve0) and (not self.ARSNextSpeedLimit or self.ARSNextSpeedLimit >= self.ARSSpeedLimit)
 end
 
-function ENT:Get325HzAproove0()
-	if self.OverrideTrackOccupied or not self.ARSSpeedLimit then return false end
-	return self.ARSSpeedLimit == 0 and self.Approve0
-end
+
 
 function ENT:GetMaxARS()
 	local ARSCodes = self.Routes[1].ARSCodes
 	if not self.Routes[1] or not ARSCodes then return 1 end
 	return tonumber(ARSCodes[#ARSCodes]) or 1
+end
+function ENT:GetSpeedLimit() --for compatibility reasons, we need to maintain the original function name. That doesn't mean we can't make a proxy with a more memorable name.
+	self:GetMaxArs()
 end
 function ENT:GetMaxARSNext()
 	local Routes = self.NextSignalLink and self.NextSignalLink.Routes or self.Routes
@@ -199,16 +65,22 @@ function ENT:GetMaxARSNext()
 end
 
 function ENT:CheckOccupation()
+	--print(self.FoundedAll)
+	--if not self.FoundedAll then return end
 
-	if self.Node and self.TrackPosition then
-		self.Occupied,self.OccupiedBy,self.OccupiedByNow = Metrostroi.IsTrackOccupied(self.Node, self.TrackPosition.x,self.TrackPosition.forward,self.ARSOnly and "ars" or "light", self)
+	if self.Node and  self.TrackPosition then
+		self.Occupied,self.OccupiedBy,self.OccupiedByNow = Metrostroi.IsTrackOccupied(self.Node, self.TrackPosition.x,self.TrackPosition.forward,"light", self)
+	end
+	if self.OccupiedByNowOld ~= self.OccupiedByNow then
+		self.OccupiedByNowOld = self.OccupiedByNow
 	end
 
 end
+
 function ENT:ARSLogic(tim)
 
 
-	if not self.Routes or not self.NextSignals then return end
+	if not self.NextSignals then return end
 	-- Check track occuping
 	if not self.Routes[self.Route or 1].Repeater  then
 		self:CheckOccupation()
@@ -309,12 +181,12 @@ function ENT:ARSLogic(tim)
 end
 
 function ENT:Think()
-
+	self.Aspect = "H0"
 
 	self:SetNW2String("Type",self.SignalType)
 
+	timer.Simple(30,function()self:SendUpdate()end)
 
-	self:SendUpdate()
 	self:NextThink(CurTime() + 0.25)
 end
 
