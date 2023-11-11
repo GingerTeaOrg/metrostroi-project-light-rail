769492a80b3a1487e672a7bd96ca036bff36d1f0
lillywho <lillywho@outlook.de>
2023-11-11 10:16:22 +0100

    Update Rheinbahn skin

--- lua/metrostroi/skins/u2h.lua
+++ lua/metrostroi/skins/u2h.lua
@@ -1,260 +1,223 @@
-Metrostroi.AddSkin("train","Def_U2SW",{
-    name = "1968 Stadtwerke",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/main",
-        decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-        
-    }
+Metrostroi.AddSkin("train", "Def_U2SW",
+                   {name = "1968 Stadtwerke", typ = "U2h", textures = {main = "models/lilly/uf/u2/u2h/main", decals = "models/lilly/uf/u2/u2h/decals-stadtwerke"}})
+
+Metrostroi.AddSkin("train", "Def_U2FVV", {name = "1968 FVV", typ = "U2h", textures = {decals = "models/lilly/uf/u2/u2h/decals-FVV"}})
+Metrostroi.AddSkin("train", "Def_U2FVV_b", {name = "1968 FVV", typ = "U2hb", textures = {decals = "models/lilly/uf/u2/u2h/decals-FVV"}})
+
+Metrostroi.AddSkin("train", "pride", {
+	name = "LGBTQ Pride because fuck bigots",
+	typ = "U2h",
+	textures = {main = "models/lilly/uf/u2/u2h/repaints/pride", decals = "models/lilly/uf/u2/u2h/decals-stadtwerke"}
 })
 
-Metrostroi.AddSkin("train","Def_U2FVV",{
-    name = "1968 FVV",
-    typ = "U2h",
-    textures = {
-        decals = "models/lilly/uf/u2/u2h/decals-FVV",
-        
-    }
-})
-Metrostroi.AddSkin("train","Def_U2FVV_b",{
-    name = "1968 FVV",
-    typ = "U2hb",
-    textures = {
-        decals = "models/lilly/uf/u2/u2h/decals-FVV",
-    }
+Metrostroi.AddSkin("train", "Duesseldorf", {
+	name = "Düsseldorf",
+	typ = "U2h",
+	textures = {
+		main = "models/lilly/uf/u2/u2h/main-duesseldorf",
+		main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
+        jointdecal = "models/lilly/uf/u2/u2h/decals-int-duesseldorf",
+		decals = "models/lilly/uf/u2/u2h/decals",
+		door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf"
+	}
 })
 
-Metrostroi.AddSkin("train","pride",{
-    name = "LGBTQ Pride because fuck bigots",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/repaints/pride",
-        decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-    }
+Metrostroi.AddSkin("train", "Duesseldorf_b", {
+	name = "Düsseldorf_b",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/main-duesseldorf",
+		main_b_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
+		decals = "models/lilly/uf/u2/u2h/decals",
+		main = "models/lilly/uf/u2/u2h/main-duesseldorf",
+		main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
+		door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf"
+	}
 })
 
-Metrostroi.AddSkin("train","Duesseldorf",{
-    name = "Düsseldorf",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/main-duesseldorf",
-        main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
-        decals = "models/lilly/uf/u2/u2h/decals",
-        door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf"
-    }
+Metrostroi.AddSkin("train", "Royalty", {
+	name = "Royalty",
+	typ = "U2h",
+	textures = {
+		main = "models/lilly/uf/u2/u2h/main-duesseldorf",
+		main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
+		decals = "models/lilly/uf/u2/u2h/decals",
+		jointdecal = "models/lilly/uf/u2/u2h/repaints/royaldecal",
+		door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf"
+	}
 })
 
-Metrostroi.AddSkin("train","Duesseldorf_b",{
-    name = "Düsseldorf_b",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/main-duesseldorf",
-        main_b_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
-        decals = "models/lilly/uf/u2/u2h/decals",
-        main = "models/lilly/uf/u2/u2h/main-duesseldorf",
-        main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
-        door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf"
-    }
+Metrostroi.AddSkin("train", "Royalty_b", {
+	name = "Royalty",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/main-duesseldorf",
+		main_b_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
+		decals = "models/lilly/uf/u2/u2h/decals",
+		jointdecal = "models/lilly/uf/u2/u2h/repaints/royaldecal",
+		door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf",
+		main = "models/lilly/uf/u2/u2h/main-duesseldorf",
+		main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left"
+	}
 })
 
-Metrostroi.AddSkin("train","Royalty",{
-    name = "Royalty",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/main-duesseldorf",
-        main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
-        decals = "models/lilly/uf/u2/u2h/decals",
-        jointdecal = "models/lilly/uf/u2/u2h/repaints/royaldecal",
-        door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf"
-    }
+Metrostroi.AddSkin("train", "pride_b", {
+	name = "LGBTQ Pride because fuck bigots",
+	typ = "U2hb",
+	textures = {main_b = "models/lilly/uf/u2/u2h/repaints/pride-b", main = "models/lilly/uf/u2/u2h/repaints/pride", decals = "models/lilly/uf/u2/u2h/decals-stadtwerke"}
 })
-
-Metrostroi.AddSkin("train","Royalty_b",{
-    name = "Royalty",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/main-duesseldorf",
-        main_b_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
-        decals = "models/lilly/uf/u2/u2h/decals",
-        jointdecal = "models/lilly/uf/u2/u2h/repaints/royaldecal",
-        door_external = "models/lilly/uf/u2/u2h/door_external_duesseldorf",
-        main = "models/lilly/uf/u2/u2h/main-duesseldorf",
-        main_left = "models/lilly/uf/u2/u2h/main-duesseldorf-left",
-    }
-})
-
-
-Metrostroi.AddSkin("train","pride_b",{
-    name = "LGBTQ Pride because fuck bigots",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/repaints/pride-b",
-        main = "models/lilly/uf/u2/u2h/repaints/pride",
-        decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-    }
-})
-Metrostroi.AddSkin("train","SVB",{
-    name = "1996",
-    typ = "U2h",
-    textures = {
+Metrostroi.AddSkin("train", "SVB", {
+	name = "1996",
+	typ = "U2h",
+	textures = {
 		main_b = "models/lilly/uf/u2/u2h/svb-main_b",
-        main = "models/lilly/uf/u2/u2h/svb-main",
-        main_left = "models/lilly/uf/u2/u2h/svb-main-left",
+		main = "models/lilly/uf/u2/u2h/svb-main",
+		main_left = "models/lilly/uf/u2/u2h/svb-main-left",
 		door_external = "models/lilly/uf/u2/u2h/door_external_svb",
 		decals = "models/lilly/uf/u2/u2h/decals-modern",
-        jointdecal = "models/lilly/uf/u2/u2h/jointdecal_vgf",
+		jointdecal = "models/lilly/uf/u2/u2h/jointdecal_vgf"
 
-    }
+	}
 })
-Metrostroi.AddSkin("train","SVB_b",{
-    name = "1996",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/svb-main_b",
-        main = "models/lilly/uf/u2/u2h/svb-main",
-        main_left = "models/lilly/uf/u2/u2h/svb-main-left",
-        main_b_left = "models/lilly/uf/u2/u2h/svb-main-left",
-		decals = "models/lilly/uf/u2/u2h/decals-modern",
-        door_external = "models/lilly/uf/u2/u2h/door_external_svb",
-    }
-})
-
-Metrostroi.AddSkin("train","SVB_SWF",{
-    name = "1996 Stadtwerke",
-    typ = "U2h",
-    textures = {
+Metrostroi.AddSkin("train", "SVB_b", {
+	name = "1996",
+	typ = "U2hb",
+	textures = {
 		main_b = "models/lilly/uf/u2/u2h/svb-main_b",
-        main = "models/lilly/uf/u2/u2h/svb-main",
-        main_left = "models/lilly/uf/u2/u2h/svb-main-left",
+		main = "models/lilly/uf/u2/u2h/svb-main",
+		main_left = "models/lilly/uf/u2/u2h/svb-main-left",
+		main_b_left = "models/lilly/uf/u2/u2h/svb-main-left",
+		decals = "models/lilly/uf/u2/u2h/decals-modern",
+		door_external = "models/lilly/uf/u2/u2h/door_external_svb"
+	}
+})
+
+Metrostroi.AddSkin("train", "SVB_SWF", {
+	name = "1996 Stadtwerke",
+	typ = "U2h",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/svb-main_b",
+		main = "models/lilly/uf/u2/u2h/svb-main",
+		main_left = "models/lilly/uf/u2/u2h/svb-main-left",
 		door_external = "models/lilly/uf/u2/u2h/door_external_svb",
 		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke2",
-        jointdecal = "models/lilly/uf/u2/u2h/jointdecal_vgf",
+		jointdecal = "models/lilly/uf/u2/u2h/jointdecal_vgf"
 
-    }
+	}
 })
-Metrostroi.AddSkin("train","SVB_SWF_b",{
-    name = "1996 Stadtwerke",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/svb-main_b",
-        main = "models/lilly/uf/u2/u2h/svb-main",
-        main_left = "models/lilly/uf/u2/u2h/svb-main-left",
-        main_b_left = "models/lilly/uf/u2/u2h/svb-main-left",
+Metrostroi.AddSkin("train", "SVB_SWF_b", {
+	name = "1996 Stadtwerke",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/svb-main_b",
+		main = "models/lilly/uf/u2/u2h/svb-main",
+		main_left = "models/lilly/uf/u2/u2h/svb-main-left",
+		main_b_left = "models/lilly/uf/u2/u2h/svb-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke2",
-        door_external = "models/lilly/uf/u2/u2h/door_external_svb",
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_svb"
+	}
 })
-Metrostroi.AddSkin("train","OrEb",{
-    name = "1981 VGF",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/orange-main",
-        main_left = "models/lilly/uf/u2/u2h/orange-main-left",
+Metrostroi.AddSkin("train", "OrEb", {
+	name = "1981 VGF",
+	typ = "U2h",
+	textures = {
+		main = "models/lilly/uf/u2/u2h/orange-main",
+		main_left = "models/lilly/uf/u2/u2h/orange-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-modern-orange",
-        door_external = "models/lilly/uf/u2/u2h/door_external_orange",
-		
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_orange"
+
+	}
 })
-Metrostroi.AddSkin("train","OrEb_b",{
-    name = "1981 VGF",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/orange_main-b",
-        main = "models/lilly/uf/u2/u2h/orange-main",
-        main_b_left = "models/lilly/uf/u2/u2h/orange-main-left",
+Metrostroi.AddSkin("train", "OrEb_b", {
+	name = "1981 VGF",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/orange_main-b",
+		main = "models/lilly/uf/u2/u2h/orange-main",
+		main_b_left = "models/lilly/uf/u2/u2h/orange-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-modern-orange",
-        door_external = "models/lilly/uf/u2/u2h/door_external_orange",
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_orange"
+	}
 })
 
-Metrostroi.AddSkin("train","OrEbSW",{
-    name = "1981 Stadtwerke",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/orange-main",
-        main_left = "models/lilly/uf/u2/u2h/orange-main-left",
+Metrostroi.AddSkin("train", "OrEbSW", {
+	name = "1981 Stadtwerke",
+	typ = "U2h",
+	textures = {
+		main = "models/lilly/uf/u2/u2h/orange-main",
+		main_left = "models/lilly/uf/u2/u2h/orange-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-        door_external = "models/lilly/uf/u2/u2h/door_external_orange",
-		
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_orange"
+
+	}
 })
-Metrostroi.AddSkin("train","OrEbSW_b",{
-    name = "1981 Stadtwerke",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/orange_main-b",
-        main = "models/lilly/uf/u2/u2h/orange-main",
-        main_left = "models/lilly/uf/u2/u2h/orange-main-left",
-        main_b_left = "models/lilly/uf/u2/u2h/orange-main-left",
+Metrostroi.AddSkin("train", "OrEbSW_b", {
+	name = "1981 Stadtwerke",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/orange_main-b",
+		main = "models/lilly/uf/u2/u2h/orange-main",
+		main_left = "models/lilly/uf/u2/u2h/orange-main-left",
+		main_b_left = "models/lilly/uf/u2/u2h/orange-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-        door_external = "models/lilly/uf/u2/u2h/door_external_orange",
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_orange"
+	}
 })
 
-Metrostroi.AddSkin("train","OrEbSW_2",{
-    name = "1981 Stadtwerke Outline",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/orange-main",
-        main_left = "models/lilly/uf/u2/u2h/orange-main-left",
+Metrostroi.AddSkin("train", "OrEbSW_2", {
+	name = "1981 Stadtwerke Outline",
+	typ = "U2h",
+	textures = {
+		main = "models/lilly/uf/u2/u2h/orange-main",
+		main_left = "models/lilly/uf/u2/u2h/orange-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke2",
-        door_external = "models/lilly/uf/u2/u2h/door_external_orange",
-		
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_orange"
+
+	}
 })
-Metrostroi.AddSkin("train","OrEbSW_2_b",{
-    name = "1981 Stadtwerke Outline",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/orange_main-b",
-        main = "models/lilly/uf/u2/u2h/orange-main",
-        main_left = "models/lilly/uf/u2/u2h/orange-main-left",
-        main_b_left = "models/lilly/uf/u2/u2h/orange-main-left",
+Metrostroi.AddSkin("train", "OrEbSW_2_b", {
+	name = "1981 Stadtwerke Outline",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/orange_main-b",
+		main = "models/lilly/uf/u2/u2h/orange-main",
+		main_left = "models/lilly/uf/u2/u2h/orange-main-left",
+		main_b_left = "models/lilly/uf/u2/u2h/orange-main-left",
 		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke2",
-        door_external = "models/lilly/uf/u2/u2h/door_external_orange",
-    }
+		door_external = "models/lilly/uf/u2/u2h/door_external_orange"
+	}
 })
 
-Metrostroi.AddSkin("train","White",{
-    name = "White",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/white",
-        main_left = "models/lilly/uf/u2/u2h/white-left",
-		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-
-    },
+Metrostroi.AddSkin("train", "White", {
+	name = "White",
+	typ = "U2h",
+	textures = {main = "models/lilly/uf/u2/u2h/white", main_left = "models/lilly/uf/u2/u2h/white-left", decals = "models/lilly/uf/u2/u2h/decals-stadtwerke"}
 })
-Metrostroi.AddSkin("train","White_b",{
-    name = "White",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/white",
-        main = "models/lilly/uf/u2/u2h/white",
-        main_left = "models/lilly/uf/u2/u2h/white-left",
-		decals = "models/lilly/uf/u2/u2h/decals-modern",
-    },
+Metrostroi.AddSkin("train", "White_b", {
+	name = "White",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/white",
+		main = "models/lilly/uf/u2/u2h/white",
+		main_left = "models/lilly/uf/u2/u2h/white-left",
+		decals = "models/lilly/uf/u2/u2h/decals-modern"
+	}
 })
 
-Metrostroi.AddSkin("train","SanDiego",{
-    name = "San Diego",
-    typ = "U2h",
-    textures = {
-        main = "models/lilly/uf/u2/u2h/san diego",
-        main_left = "models/lilly/uf/u2/u2h/san_diego_left",
-		decals = "models/lilly/uf/u2/u2h/decals-stadtwerke",
-
-    },
+Metrostroi.AddSkin("train", "SanDiego", {
+	name = "San Diego",
+	typ = "U2h",
+	textures = {main = "models/lilly/uf/u2/u2h/san diego", main_left = "models/lilly/uf/u2/u2h/san_diego_left", decals = "models/lilly/uf/u2/u2h/decals-stadtwerke"}
 })
-Metrostroi.AddSkin("train","SanDiego_b",{
-    name = "San Diego",
-    typ = "U2hb",
-    textures = {
-        main_b = "models/lilly/uf/u2/u2h/san diego_b",
-        main = "models/lilly/uf/u2/u2h/san diego",
-        --main_left = "models/lilly/uf/u2/u2h/white-left",
-		decals = "models/lilly/uf/u2/u2h/decals-modern",
-    },
+Metrostroi.AddSkin("train", "SanDiego_b", {
+	name = "San Diego",
+	typ = "U2hb",
+	textures = {
+		main_b = "models/lilly/uf/u2/u2h/san diego_b",
+		main = "models/lilly/uf/u2/u2h/san diego",
+		-- main_left = "models/lilly/uf/u2/u2h/white-left",
+		decals = "models/lilly/uf/u2/u2h/decals-modern"
+	}
 })
 --[[
 
