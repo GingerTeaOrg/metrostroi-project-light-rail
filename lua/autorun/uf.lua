75491708f82940c049df0476a2fc3f8f0038025f
lillywho <lillywho@outlook.de>
2023-11-30 09:38:19 +0100

    Fix clientside file execution

--- lua/autorun/uf.lua
+++ lua/autorun/uf.lua
@@ -402,10 +402,9 @@ if SERVER then
         include("uf/" .. filename)
     end
 end
-if CLIENT then
-    files = file.Find("uf/cl_*.lua", "LUA")
-    for _, filename in pairs(files) do
-        AddCSLuaFile("uf/" .. filename)
-        include("uf/" .. filename)
-    end
+
+files = file.Find("uf/cl_*.lua", "LUA")
+for _, filename in pairs(files) do
+    AddCSLuaFile("uf/" .. filename)
+    include("uf/" .. filename)
 end
