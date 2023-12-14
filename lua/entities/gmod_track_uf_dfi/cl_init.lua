df7006b21df0319f9741b399aa268289e644e932
lillywho <lillywho@outlook.de>
2023-12-14 13:31:27 +0100

    dfi clientside clock

--- lua/entities/gmod_track_uf_dfi/cl_init.lua
+++ lua/entities/gmod_track_uf_dfi/cl_init.lua
@@ -110,29 +110,40 @@ end
 
 
 function ENT:ClockFace()
-	local serverTime = os.time() -- Get the server time in UTC
-	local gmtPlus1Offset = 1 * 60 * 60 -- GMT+1 in seconds
-	local gmtPlus1Time = serverTime + gmtPlus1Offset
 	
-	-- Format the time into hours and minutes
-	local hours = tonumber(os.date("%H", gmtPlus1Time))
-	local minutes = tonumber(os.date("%M", gmtPlus1Time))
+	local Time = self:GetNW2String("Time","0000")
+	local hours = tonumber(string.sub(Time,1,2),10)
+	local minutes = tonumber(string.sub(Time,3,4),10)
 	
-	self.Hours:SetPoseParameter("position",60 / hours * 100)
-	self.Minutes:SetPoseParameter("position",60 / minutes * 100)
+	self.MinutePos = ((minutes / 60) * 100) + 2
+	self.HourPos = ((hours / 12) * 100 -4) + ((((minutes / 60) * 100) + 2)) / 12
+	
+	self.Hours:SetPoseParameter("position",self.HourPos)
+	self.Hours:InvalidateBoneCache()
+	self.Minutes:SetPoseParameter("position",self.MinutePos)
+	self.Minutes:InvalidateBoneCache()
+	
+	
 end
 
 
 
 
-function ENT:Initialize() 
-	self.DFI = self:CreateRT("DFI", 10000, 10000)
-	self.Overlay = CreateMaterial( "Overlay", "VertexLitGeneric", {
-		["$basetexture"] = "color/rgb/000",
-		["$vertexcolor"] = 1,
-	} )
-	self.Hours = ents.CreateClientProp("lilly/uf/stations/dfi_hands_hours.mdl")
-	self.Minutes = ents.CreateClientProp("lilly/uf/stations/dfi_hands_minutes.mdl")
+function ENT:Initialize()
+	
+	local ang = self:GetAngles() - Angle(0,0,-8)
+
+	local pos = self:GetPos()  --+ Vector(0,0,-14)
+	
+	self.Hours = ents.CreateClientProp("models/lilly/uf/stations/dfi_hands_hours.mdl")
+	self.Minutes = ents.CreateClientProp("models/lilly/uf/stations/dfi_hands_minutes.mdl")
+
+	self.Hours:SetPos(pos)
+	self.Hours:SetAngles(ang)
+	self.Minutes:SetPos(pos)
+	self.Minutes:SetAngles(ang)
+	self.Hours:SetParent(self)
+	self.Minutes:SetParent(self)
 	self.Hours:Spawn()
 	self.Minutes:Spawn()
 	
@@ -143,14 +154,16 @@ function ENT:Initialize()
 	
 	
 	self.AnnouncementPlayed = false
+	--todo: Introduce a table so that this can be made more flexible. Hardcoding is nono.
+	self.Abbreviations = {	
+		["Ldstr"] = "Landstr.", 
+		["Pl"] = "Platz",
+		["Hhmrk"] = "Hohemark",
+		["Bmmrsh"] = "Bommersheim",
+	}	
 	
-	self.Abbreviations = {	["Ldstr"] = "Landstr.", --FIXME: Introduce a table so that this can be made more flexible. Hardcoding is nono.
-	["Pl"] = "Platz",
-	["Hhmrk"] = "Hohemark",
-	["Bmmrsh"] = "Bommersheim",
-}
 
-
+	
 end
 
 function ENT:PrintText(x, y, text, font)
@@ -163,13 +176,29 @@ function ENT:PrintText(x, y, text, font)
 end
 
 function ENT:Think()
+	self:ClockFace()
 
 	
 	
-	self:ClockFace()
 	
 	local mode = self:GetNW2Int("Mode", 0)
 	self.Theme = self:GetNW2String("Theme","Frankfurt")
+
+
+	self.Train1Time = self:GetNW2String("Train1Time", "E")
+	self.Train2Time = self:GetNW2String("Train2Time", "E")
+	self.Train3Time = self:GetNW2String("Train3Time", "E")
+	self.Train4Time = self:GetNW2String("Train4Time", "E")
+	
+	self.Train1Destination = self:GetNW2String("Train1Destination", "ERROR")
+	self.Train2Destination = self:GetNW2String("Train2Destination", "ERROR")
+	self.Train3Destination = self:GetNW2String("Train3Destination", "ERROR")
+	self.Train4Destination = self:GetNW2String("Train4Destination", "ERROR")
+	
+	self.Train1Entry = self:GetNW2Bool("Train1Entry",false)
+	self.Train2Entry = self:GetNW2Bool("Train2Entry",false)
+	self.Train3Entry = self:GetNW2Bool("Train3Entry",false)
+	self.Train4Entry = self:GetNW2Bool("Train4Entry",false)
 	
 	
 	if self.Theme == "Frankfurt" or self.Theme == "Essen" or self.Theme == "Duesseldorf" then
@@ -220,9 +249,9 @@ function ENT:Think()
 	end
 	if mode == 2 and self.AnnouncementPlayed == false then
 		self.AnnouncementPlayed = true
-		if self.Theme == "Frankfurt" then
-			if self.Destination1 ~= "Leerfahrt" and self.Destination1 ~= "PROBEWAGEN NICHT EINSTEIGEN" and self.Destination1 ~= "FAHRSCHULE NICHT EINSTEIGEN" and self.Destination1 ~= "SONDERWAGEN NICHT EINSTEIGEN" and self.Destination1 ~= " " and self.Destination1 then
-				self:PlayOnceFromPos("lilly/uf/DFI/frankfurt/"..self.LineString1.." ".."Richtung".." "..self.Destination1..".mp3", 2, 1, 1, 1, self:GetPos())
+		if self.Theme == "Frankfurt" then --todo implement other themes
+			if self.Train1Destination and self.Train1Destination ~= "Leerfahrt" and self.Train1Destination ~= "PROBEWAGEN NICHT EINSTEIGEN" and self.Train1Destination ~= "FAHRSCHULE NICHT EINSTEIGEN" and self.Train1Destination ~= "SONDERWAGEN NICHT EINSTEIGEN" and self.Train1Destination ~= " " then
+				self:PlayOnceFromPos("lilly/uf/DFI/frankfurt/"..self.LineString1.." ".."Richtung".." "..self.Train1Destination1..".mp3", 2, 1, 1, 1, self:GetPos())
 			else
 				self:PlayOnceFromPos("lilly/uf/DFI/frankfurt/Bitte Nicht Einsteigen.mp3", 2, 1, 1, 1, self:GetPos())
 			end
@@ -232,26 +261,18 @@ function ENT:Think()
 	end
 	
 	
-	self.Train1Time = self:GetNW2String("Train1Time", "E")
-	self.Train2Time = self:GetNW2String("Train2Time", "E")
-	self.Train3Time = self:GetNW2String("Train3Time", "E")
-	self.Train4Time = self:GetNW2String("Train4Time", "E")
+
 	
-	self.Train1Destination = self:GetNW2String("Train1Destination", "ERROR")
-	self.Train2Destination = self:GetNW2String("Train2Destination", "ERROR")
-	self.Train3Destination = self:GetNW2String("Train3Destination", "ERROR")
-	self.Train4Destination = self:GetNW2String("Train4Destination", "ERROR")
-	
-	self.Train1Entry = self:GetNW2Bool("Train1Entry",false)
-	self.Train2Entry = self:GetNW2Bool("Train2Entry",false)
-	self.Train3Entry = self:GetNW2Bool("Train3Entry",false)
-	self.Train4Entry = self:GetNW2Bool("Train4Entry",false)
-	
-	self.Train1Destination = self:SubstituteAbbreviation(self.Train1Destination) or self.Train1Destination
-	
+	self.Train1DestinationString = self:SubstituteAbbreviation(self.Train1Destination) and self:SubstituteAbbreviation(self.Train1Destination) or self.Train1Destination
+	self.Train2DestinationString = self:SubstituteAbbreviation(self.Train2Destination) and self:SubstituteAbbreviation(self.Train2Destination) or self.Train2Destination
+	self.Train3DestinationString = self:SubstituteAbbreviation(self.Train3Destination) and self:SubstituteAbbreviation(self.Train3Destination) or self.Train3Destination
+	self.Train4DestinationString = self:SubstituteAbbreviation(self.Train4Destination) and self:SubstituteAbbreviation(self.Train4Destination) or self.Train4Destination
 end
 
 function ENT:SubstituteAbbreviation(Input)
+	local output = nil
+	if Input == "ERROR" then return end
+	if not self.Abbreviations then return nil end
 	for k,v in pairs(self.Abbreviations) do
 		if string.find(Input,k,1,true) then
 			output = string.gsub(Input,k,self.Abbreviations[k],1)
@@ -273,7 +294,7 @@ function ENT:Draw()
 		local ang2 = self:LocalToWorldAngles(Angle(0, 180, 95.6))
 		cam.Start3D2D(pos, ang, 0.03)
 		self:PrintText(-8, 0, self.LineString1, "Lumino_Big")
-		self:PrintText(-5.1, 0, self.Train1Destination, "Lumino_Big")
+		self:PrintText(-5.1, 0, self.Train1DestinationString, "Lumino_Big")
 		--self:PrintText(-5, 6, self:GetNW2String("TrainVia", "über Testplatz"), "Lumino")
 		self:PrintText(10, 11.6, string.rep("ó",self:GetNW2Int("Train1ConsistLength", 1)), "Lumino_Cars")
 		self:PrintText(10.3, 12.5, "____", "Lumino")
@@ -281,12 +302,12 @@ function ENT:Draw()
 		cam.End3D2D()
 		cam.Start3D2D(pos2, ang2, 0.03)
 		self:PrintText(-8, 0, self.LineString1, "Lumino_Big")
-		self:PrintText(-5.1, 0, self.Train1Destination, "Lumino_Big")
+		self:PrintText(-5.1, 0, self.Train1DestinationString, "Lumino_Big")
 		--self:PrintText(-5, 6, self:GetNW2String("TrainVia", "über Testplatz"), "Lumino")
 		self:PrintText(10, 11.6, string.rep("ó",self:GetNW2Int("Train1ConsistLength", 1)), "Lumino_Cars")
 		self:PrintText(10.3, 12.5, "____", "Lumino")
 		self:PrintText(9.1, 13.1, ":", "Lumino")
-
+		
 		cam.End3D2D()
 	elseif mode == 1 then
 		
@@ -298,7 +319,7 @@ function ENT:Draw()
 		
 		---------------------------------------------------------------------------------
 		self:PrintText(-1.5, 0, self.LineString1, "Lumino_Big")
-		self:PrintText(1, 0, self.Train1Destination, "Lumino")
+		self:PrintText(1, 0, self.Train1DestinationString, "Lumino")
 		if #self.Train1Time == 2 then
 			self:PrintText(25, 0, self:GetNW2String("Train1Time", "10"), "Lumino")
 		elseif #self.Train1Time == 1 then
@@ -307,7 +328,7 @@ function ENT:Draw()
 		---------------------------------------------------------------------
 		if self.Train2Entry == true then
 			self:PrintText(-1.5, 6, self.LineString2, "Lumino_Big")
-			self:PrintText(1, 6, self.Train2Destination, "Lumino")
+			self:PrintText(1, 6, self.Train2DestinationString, "Lumino")
 			if #self.Train2Time == 2 then
 				self:PrintText(25, 6, self.Train2Time, "Lumino")
 			elseif #self.Train2Time == 1 then
@@ -317,7 +338,7 @@ function ENT:Draw()
 		----------------------------------------------------------------------
 		if self.Train3Entry == true then
 			self:PrintText(-1.5, 12, self.LineString3, "Lumino_Big")
-			self:PrintText(1, 12, self.Train3Destination, "Lumino")
+			self:PrintText(1, 12, self.Train3DestinationString, "Lumino")
 			if #self.Train3Time == 2 then
 				self:PrintText(25, 12, self.Train3Time, "Lumino")
 			elseif #self.Train3Time == 1 then
@@ -327,7 +348,7 @@ function ENT:Draw()
 		-------------------------------------------------------------------------------
 		if self.Train4Entry == true then
 			self:PrintText(-1.5, 18, self.LineString4, "Lumino_Big")
-			self:PrintText(1, 18, self.Train4Destination, "Lumino")
+			self:PrintText(1, 18, self.Train4DestinationString, "Lumino")
 			if #self.Train4Time == 2 then
 				self:PrintText(25, 18, self.Train4Time, "Lumino")
 			elseif #self.Train4Time == 1 then
@@ -340,7 +361,7 @@ function ENT:Draw()
 		
 		---------------------------------------------------------------------------------
 		self:PrintText(-1.5, 0, self.LineString1, "Lumino_Big")
-		self:PrintText(1, 0, self.Train1Destination, "Lumino")
+		self:PrintText(1, 0, self.Train1DestinationString, "Lumino")
 		if #self.Train1Time == 2 then
 			self:PrintText(25, 0, self:GetNW2String("Train1Time", "10"), "Lumino")
 		elseif #self.Train1Time == 1 then
@@ -349,7 +370,7 @@ function ENT:Draw()
 		---------------------------------------------------------------------
 		if self.Train2Entry == true then
 			self:PrintText(-1.5, 6, self.LineString2, "Lumino_Big")
-			self:PrintText(1, 6, self.Train2Destination, "Lumino")
+			self:PrintText(1, 6, self.Train2DestinationString, "Lumino")
 			if #self.Train2Time == 2 then
 				self:PrintText(25, 6, self.Train2Time, "Lumino")
 			elseif #self.Train2Time == 1 then
@@ -359,7 +380,7 @@ function ENT:Draw()
 		----------------------------------------------------------------------
 		if self.Train3Entry == true then
 			self:PrintText(-1.5, 12, self.LineString3, "Lumino_Big")
-			self:PrintText(1, 12, self.Train3Destination, "Lumino")
+			self:PrintText(1, 12, self.Train3DestinationString, "Lumino")
 			if #self.Train3Time == 2 then
 				self:PrintText(25, 12, self.Train3Time, "Lumino")
 			elseif #self.Train3Time == 1 then
@@ -369,7 +390,7 @@ function ENT:Draw()
 		-------------------------------------------------------------------------------
 		if self.Train4Entry == true then
 			self:PrintText(-1.5, 18, self.LineString4, "Lumino_Big")
-			self:PrintText(1, 18, self.Train4Destination, "Lumino")
+			self:PrintText(1, 18, self.Train4DestinationString, "Lumino")
 			if #self.Train4Time == 2 then
 				self:PrintText(25, 18, self.Train4Time, "Lumino")
 			elseif #self.Train4Time == 1 then
@@ -412,7 +433,7 @@ function ENT:Draw()
 		})
 		
 		cam.End3D2D()
-
+		
 	end
 	
 end
