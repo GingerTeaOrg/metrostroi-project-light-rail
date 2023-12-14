adf56211e3c2d23a1262a9cffb4f216603501e0d
lillywho <lillywho@outlook.de>
2023-12-14 13:29:41 +0100

    Custom model support for signal server

--- lua/entities/gmod_mplr_signalserver/init.lua
+++ lua/entities/gmod_mplr_signalserver/init.lua
@@ -6,10 +6,13 @@ function ENT:Initialize()
     if #ent > 1 then
         SafeRemoveEntity(self)
     end
+
+    self:SetModel(self.Model or "")
+    if not self.Model then self:SetRenderMode(RENDERMODE_TRANSALPHA) self:SetColor(Color(0, 0, 0, 0)) end --if we haven't been given a proper model, just hide
 end
 function ENT:Think()
     self.BaseClass.Think()
-    UF.UpdateSignalBlocks()
+
 
     UF.UpdateSignalBlockOccupation()
     self:NextThink(CurTime() + 0.25)
