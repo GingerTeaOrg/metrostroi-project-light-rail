c6ddde60de759b032e93eb286254a1e01c893732
lillywho <lillywho@outlook.de>
2023-12-20 09:49:30 +0100

    Prep for modular coupler and bogey setup

---
+++ lua/uf/trainequipment/example.lua
@@ -0,0 +1,14 @@
+--[[
+
+local MyBogey = {
+    u5={
+            "models/lilly/uf/u5/bogey.mdl",
+            Vector(0,0.0,0),Angle(0,0,0),nil,
+            Vector(0,-61,-14),Vector(0,61,-14),
+            nil,
+            Vector(4.3,-63,-3.3),Vector(4.3,63,-3.3),
+        },
+}
+
+table.Add(UF.BogeyTypes,MyBogey)
+]]
