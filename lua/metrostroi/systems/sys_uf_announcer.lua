50af48d411a29a853e76826a91d93a1a760b513f
lillywho <lillywho@outlook.de>
2024-02-09 16:21:59 +0100

    Reformat announcer

--- lua/metrostroi/systems/sys_uf_announcer.lua
+++ lua/metrostroi/systems/sys_uf_announcer.lua
@@ -8,13 +8,9 @@ Metrostroi.DefineSystem("uf_announcer")
 TRAIN_SYSTEM.DontAccelerateSimulation = true
 local ANNOUNCER_CACHE_LIMIT = 30
 
-function TRAIN_SYSTEM:Outputs()
-    return {}
-end
+function TRAIN_SYSTEM:Outputs() return {} end
 
-function TRAIN_SYSTEM:Inputs()
-    return {"Reset"}
-end
+function TRAIN_SYSTEM:Inputs() return {"Reset"} end
 
 if TURBOSTROI then return end
 
@@ -27,19 +23,17 @@ if SERVER then
 
     util.AddNetworkString("uf_announcer")
 
-    function TRAIN_SYSTEM:TriggerInput(name,value)
+    function TRAIN_SYSTEM:TriggerInput(name, value)
         if name == "Reset" then
             self:Reset()
             self.AnnTable = value
         end
-        if name == "Table" then
-            self.AnnTable = value
-        end
+        if name == "Table" then self.AnnTable = value end
     end
 
     function TRAIN_SYSTEM:Queue(tbl)
         for k, v in pairs(tbl) do
-            if v~=-2 then
+            if v ~= -2 then
                 table.insert(self.Schedule, tbl and tbl[v] or v)
             else
                 self:Reset()
@@ -63,79 +57,79 @@ if SERVER then
             net.Broadcast()
         end
     end
-function TRAIN_SYSTEM:dumpTable(table, indent)
-  indent = indent or 0
-  for key, value in pairs(table) do
-    if type(value) == "table" then
-      print(string.rep("  ", indent) .. key .. " = {")
-      self:dumpTable(value, indent + 1)
-      print(string.rep("  ", indent) .. "}")
-    else
-      print(string.rep("  ", indent) .. key .. " = " .. tostring(value))
+    function TRAIN_SYSTEM:dumpTable(table, indent)
+        indent = indent or 0
+        for key, value in pairs(table) do
+            if type(value) == "table" then
+                print(string.rep("  ", indent) .. key .. " = {")
+                self:dumpTable(value, indent + 1)
+                print(string.rep("  ", indent) .. "}")
+            else
+                print(string.rep("  ", indent) .. key .. " = " ..
+                          tostring(value))
+            end
+        end
     end
-  end
-end
-    --end
+    -- end
     function TRAIN_SYSTEM:Think()
         if #self.Schedule > 0 and not self.Playing then -- if announcements are queued and are not marked as playing yet
-            for i = 1, #self.Train.WagonList do --for every car in the consist
-                self.Train.WagonList[i]:SetNW2Bool("AnnouncerPlaying", true) --mark the announcements as active
+            for i = 1, #self.Train.WagonList do -- for every car in the consist
+                self.Train.WagonList[i]:SetNW2Bool("AnnouncerPlaying", true) -- mark the announcements as active
             end
-            self.Playing = true --note down the accounements as playing for us locally
-        elseif #self.Schedule == 0 and self.Playing and not self.AnnounceTimer then --announcements aren't queued
+            self.Playing = true -- note down the accounements as playing for us locally
+        elseif #self.Schedule == 0 and self.Playing and not self.AnnounceTimer then -- announcements aren't queued
             for i = 1, #self.Train.WagonList do
-                self.Train.WagonList[i]:SetNW2Bool("AnnouncerPlaying", false) --we're not playing anything anymore
+                self.Train.WagonList[i]:SetNW2Bool("AnnouncerPlaying", false) -- we're not playing anything anymore
             end
-            self.Playing = false --we're not playing anything anymore
+            self.Playing = false -- we're not playing anything anymore
         end
 
-        while #self.Schedule > 0 and (not self.AnnounceTimer or CurTime() - self.AnnounceTimer > 0) do
+        while #self.Schedule > 0 and
+            (not self.AnnounceTimer or CurTime() - self.AnnounceTimer > 0) do
             self:dumpTable(self.Schedule, 5)
             local tbl = table.remove(self.Schedule, 1)
-            self:dumpTable(tbl,5)
+            self:dumpTable(tbl, 5)
             if type(tbl) == "number" then
                 if tbl == -1 then
                 else
                     self.AnnounceTimer = CurTime() + tbl
                 end
             elseif type(tbl) == "table" then
-                for k,v in pairs(tbl) do
+                for k, v in pairs(tbl) do
                     self:WriteMessage(k)
                     self.AnnounceTimer = CurTime() + v
                 end
             else
-                ErrorNoHalt("Announcer error in message "..tbl.."\n")
+                ErrorNoHalt("Announcer error in message " .. tbl .. "\n")
             end
         end
-        if #self.Schedule == 0 and self.AnnounceTimer and CurTime() - self.AnnounceTimer > 0 then
-            self.AnnounceTimer = nil --no need to run a timer when self.Schedule is null
-            
+        if #self.Schedule == 0 and self.AnnounceTimer and CurTime() -
+            self.AnnounceTimer > 0 then
+            self.AnnounceTimer = nil -- no need to run a timer when self.Schedule is null
+
         end
-        if #self.Schedule > ANNOUNCER_CACHE_LIMIT then
-            self:Reset()
-        end
+        if #self.Schedule > ANNOUNCER_CACHE_LIMIT then self:Reset() end
     end
 else
     net.Receive("uf_announcer", function(len, pl)
         local train = net.ReadEntity()
         if not IsValid(train) or not train.RenderClientEnts then return end
         local snd = net.ReadString()
-        --print(snd)
+        -- print(snd)
         if train.AnnouncerPositions then
-            
+
             for k, v in ipairs(train.AnnouncerPositions) do
-                print("with announcerpos")
-                train:PlayOnceFromPos("announcer" .. k, snd, 0.4, 1,400, 1e9, v[1])
+                train:PlayOnceFromPos("announcer" .. k, snd, 0.4, 1, 400, 1e9,
+                                      v[1])
             end
         else
-            print("without announcerpos")
-            train:PlayOnceFromPos("announcer", snd, train.OnAnnouncer and train:OnAnnouncer(1) or 0.4, 1, 600, 1e9, Vector(0, 0, 0))
+            train:PlayOnceFromPos("announcer", snd, train.OnAnnouncer and
+                                      train:OnAnnouncer(1) or 0.4, 1, 600, 1e9,
+                                  Vector(0, 0, 0))
         end
     end)
 
-    function TRAIN_SYSTEM:ClientInitialize()
-    end
+    function TRAIN_SYSTEM:ClientInitialize() end
 
-    function TRAIN_SYSTEM:ClientThink()
-    end
+    function TRAIN_SYSTEM:ClientThink() end
 end
