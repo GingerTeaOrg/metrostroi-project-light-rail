cfaf4c2c8a6ae4d10522a2a844ff3994f2e926e6
lillywho <lillywho@outlook.de>
2024-05-26 16:43:19 +0200

    Update example routing table

--- lua/uf/routing/example.lua
+++ lua/uf/routing/example.lua
@@ -1,4 +1,2 @@
---Format: LineRoute = {[SwitchID] = "Direction"}
-UF.RoutingTable = {
-    ["0102"] = { ["100"] = "Left",},
-}
+-- Format: LineRoute = {[SwitchID] = "Direction"}
+UF.RoutingTable = {["0102"] = {[1] = "Left"}}
