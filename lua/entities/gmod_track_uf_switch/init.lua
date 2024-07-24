cedde5d0b62bc2e49645e7cde72c212db914326a
lillywho <lillywho@outlook.de>
2024-07-24 10:07:51 +0200

    SwitchEnt/TriggerEnt: Format and implement queued commands

--- lua/entities/gmod_track_uf_switch/init.lua
+++ lua/entities/gmod_track_uf_switch/init.lua
@@ -1,67 +1,83 @@
-AddCSLuaFile("shared.lua")
-include("shared.lua")
-
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" )
 function ENT:Initialize()
-	self:SetModel("models/metrostroi/signals/box.mdl")
-	Metrostroi.DropToFloor(self)
-
+	self:SetModel( "models/metrostroi/signals/box.mdl" )
+	Metrostroi.DropToFloor( self )
 	self.ID = self.VMF.ID
 	-- Initial state of the switch
 	self.AlternateTrack = false
 	self.AlreadyLocked = false
-
 	self.InhibitSwitching = false
 	self.SignalCaller = {}
 	self.LastSignalTime = 0
-
 	self.Left = self.VMF.PositionCorrespondance or "alt"
-
-	local list = ents.FindInSphere(self:GetPos(), 128)
+	local list = ents.FindInSphere( self:GetPos(), 128 )
 	self.TrackSwitches = {}
-	for k, v in pairs(list) do
-		if (v:GetClass() == "prop_door_rotating")
-					and (string.find(v:GetName(), "blade") or string.find(v:GetName(), "switch") or string.find(v:GetName(), "swh") or string.find(v:GetName(), "swit")) then
-			table.insert(self.TrackSwitches, v)
-
-			timer.Simple(0.05, function() debugoverlay.Line(v:GetPos(), self:GetPos(), 10, Color(255, 255, 0), true) end)
+	for k, v in pairs( list ) do
+		if ( v:GetClass() == "prop_door_rotating" ) and ( string.find( v:GetName(), "blade" ) or string.find( v:GetName(), "switch" ) or string.find( v:GetName(), "swh" ) or string.find( v:GetName(), "swit" ) ) then
+			table.insert( self.TrackSwitches, v )
+			timer.Simple( 0.05, function() debugoverlay.Line( v:GetPos(), self:GetPos(), 10, Color( 255, 255, 0 ), true ) end )
 		end
 	end
-	self.AntiSpam = 0
+
+	self.Queue = {}
+	self.QueueSet = {}
 	UF.UpdateSignalEntities()
 end
 
-function ENT:OnRemove() UF.UpdateSignalEntities() end
+function ENT:OnRemove()
+	UF.UpdateSignalEntities()
+end
 
 function ENT:Occupied()
-	local pos = self.TrackPosition
+	local pos = self.TrackPositionition
 	if not pos then return end
-	local trackOccupied = Metrostroi.IsTrackOccupied(pos.node1, pos.x, pos.forward, "switch")
-	if pos then self.InhibitSwitching = false end
+	local trackOccupied = Metrostroi.IsTrackOccupied( pos.node1, pos.x, pos.forward, "switch" )
+	self.InhibitSwitching = false
 end
-function ENT:Think()
-	self.TrackPosition = self.TrackPosition or Metrostroi.GetPositionOnTrack(self:GetPos())[1]
 
-	if not next(self.TrackSwitches) then return end
+function ENT:Think()
+	self.TrackPositionition = self.TrackPositionition or Metrostroi.GetPositionOnTrack( self:GetPos() )[ 1 ]
+	if not next( self.TrackSwitches ) then return end
 	self:Occupied()
 	self:Switching()
 	-- Process logic
-	self:NextThink(CurTime() + 1.0)
+	self:NextThink( CurTime() + 1.0 )
 	return true
 end
+
 function ENT:Switching()
 	if self.AlternateTrack then
-		for k, v in ipairs(self.TrackSwitches) do v:Fire("Open", "", 0, self, self) end
-
+		for _, v in ipairs( self.TrackSwitches ) do
+			v:Fire( "Open", "", 0, self, self )
+		end
 	elseif not self.AlternateTrack then
-		for k, v in ipairs(self.TrackSwitches) do v:Fire("Close", "", 0, self, self) end
-
+		for _, v in ipairs( self.TrackSwitches ) do
+			v:Fire( "Close", "", 0, self, self )
+		end
 	end
 end
-function ENT:TriggerSwitch(direction, ent)
-	print(Format("Switching to direction %s by service %s %s", direction, ent.IBIS.LineCourse, ent.IBIS.Route))
-	direction = string.lower(direction)
 
-	if self.InhibitSwitching then return false end
+function ENT:TriggerSwitch( direction, ent )
+	self:SwitchingQueue( direction, ent )
+	if ent.IBIS then
+		print( Format( "Switching to direction %s by service %s %s", direction, ent.IBIS.LineCourse, ent.IBIS.Route ) )
+	else
+		print( Format( "Switching to direction %s", direction ) )
+	end
+
+	direction = string.lower( direction )
+	if self.InhibitSwitching then
+		print( "switching blocked" )
+		return false
+	end
+
+	if self.Queue[ ent ][ 1 ] > 1 then
+		return
+	else
+		direction = self.Queue[ ent ][ 2 ]
+	end
+
 	if direction == "left" and self.Left == "alt" then
 		self.AlternateTrack = true
 	elseif direction == "left" and self.Left == "main" then
@@ -69,11 +85,27 @@ function ENT:TriggerSwitch(direction, ent)
 	elseif direction == "right" and self.Left == "alt" then
 		self.AlternateTrack = false
 	elseif direction == "right" and self.Left == "main" then
+		self.AlternateTrack = true
+	elseif direction == "main" then
 		self.AlternateTrack = false
+	elseif direction == "alt" then
+		self.AlternateTrack = true
 	end
 end
 
-function ENT:KeyValue(key, value)
+function ENT:KeyValue( key, value )
 	self.VMF = self.VMF or {}
-	self.VMF[key] = value
+	self.VMF[ key ] = value
+end
+
+function ENT:SwitchingQueue( direction, ent )
+	if self.Queue[ ent ] then
+		local TargetX = Metrostroi.GetPositionOnTrack( ent:GetPos() )[ 1 ].x
+		local trainStillHere = UF.IsTrackOccupied( self.TrackPosition.node1, self.TrackPosition.x, self.TrackPosition.x < TargetX, "light", TargetX )
+		-- if the train isn't between the trigger and the switch motor anymore, remove it from the queue
+		if not trainStillHere then self.Queue[ ent ] = nil end
+		return
+	else
+		self.Queue[ ent ] = { #self.Queue + 1, direction }
+	end
 end
--- lua/entities/gmod_track_uf_switch_contact/init.lua
+++ lua/entities/gmod_track_uf_switch_contact/init.lua
@@ -1,93 +1,129 @@
-AddCSLuaFile("cl_init.lua")
-AddCSLuaFile("shared.lua")
-include("shared.lua")
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" )
 function ENT:Initialize()
-	self:SetModel("models/lilly/uf/signage/point_contact.mdl")
-	self.TargetEnt = ents.FindByName(self.VMF.SwitchEnt)[1]
-	self.SwitchID = self.TargetEnt.ID
-	self.Invisible = self.VMF.Invisible == "1" and true or false
-	self:SetNW2Bool("Invisible", self.Invisible)
-	util.PrecacheModel("models/lilly/uf/signage/point_contact.mdl")
-	self.TrackPos = Metrostroi.GetPositionOnTrack(self:GetPos())[1]
-	self.PairedControllers = {}
-
-	self.TrainLockedIn = "none"
+	self:SetModel( "models/lilly/uf/signage/point_contact.mdl" )
+	self.TargetEnt = ents.FindByName( self.VMF.SwitchEnt )[ 1 ] -- the switch this trigger is paired to
+	self.SwitchID = self.TargetEnt.ID -- the switch ID
+	self.Invisible = self.VMF.Invisible == "1" and true or false -- on the road we can just move the sign to wherever it makes sense, in tunnels we hide it
+	self:SetNW2Bool( "Invisible", self.Invisible )
+	util.PrecacheModel( "models/lilly/uf/signage/point_contact.mdl" ) -- precache for clientModel use
+	self.TrackPos = Metrostroi.GetPositionOnTrack( self:GetPos() )[ 1 ] -- where on the rail coordinates are we
+	self.PairedControllers = {} --initialise table of paired trigger points
+	self.AutomaticSwitching = self.VMF.PositionCorrespondanceOppositeDir -- what side to automatically switch to if a train approaches
 end
+
 function ENT:GetPairedControllers()
-	if not type(self.VMF.PairedControllerLeft) == "string" or not type(self.VMF.PairedControllerRight) == "string" then return end
 	if self.VMF.PairedControllerLeft then
-		local controller1 = ents.FindByName(self.VMF.PairedControllerLeft[1])
-		table.insert(self.PairedControllers, 1, {controller1, self.VMF.PairedControllerLeftCommand})
-	elseif self.VMF.PairedControllerRight then
-		local controller2 = self.VMF.PairedControllerRight[1]
-		table.insert(self.PairedControllers, 2, {controller2, self.VMF.PairedControllerRightCommand})
+		local controller1 = ents.FindByName( self.VMF.PairedControllerLeft[ 1 ] )
+		table.insert( self.PairedControllers, 1, { controller1, self.VMF.PairedControllerLeftCommand } )
+	end
+
+	if self.VMF.PairedControllerRight then
+		local controller2 = self.VMF.PairedControllerRight[ 1 ]
+		table.insert( self.PairedControllers, 2, { controller2, self.VMF.PairedControllerRightCommand } )
 	end
 end
+
 function ENT:Think()
-	self.TrackPos = self.TrackPos or Metrostroi.GetPositionOnTrack(self:GetPos())[1] -- track location data because it doesn't work in Initialize() for some reason
-	if not IsValid(self.TargetEnt) then return end
-	if not self.TargetEnt.TrackPosition then return end
-	if not self.VMF.PairedControllerLeft or self.VMF.PairedControllerRight then return end
-	if not self.PairedControllers[1] then self:GetPairedControllers() end
+	local function runSwitchingNormal( line, route )
+		local routeCommand = UF.RoutingTable[ line .. route ] and UF.RoutingTable[ line .. route ][ self.SwitchID ] or nil
+		if next( self.PairedControllers ) then local controllerLeft, controllerRight = self.PairedControllers[ 1 ][ 1 ], self.PairedControllers[ 2 ][ 2 ] end
+		if routeCommand then Target:TriggerSwitch( UF.RoutingTable[ line .. route ][ self.SwitchID ], train ) end
+		if routeCommand == "left" and controllerLeft then
+			self:TriggerPairedController( self.PairedControllers[ 1 ][ 2 ], self.PairedControllers[ 1 ][ 1 ] )
+		elseif routeCommand == "right" and controllerRight then
+			self:TriggerPairedController( self.PairedControllers[ 2 ][ 2 ], self.PairedControllers[ 2 ][ 1 ] )
+		end
+	end
+
+	local function runSwitching()
+		local routeCommand = UF.RoutingTable[ line .. route ] and UF.RoutingTable[ line .. route ][ self.SwitchID ] or nil
+		local controllerLeft, controllerRight = self.PairedControllers[ 1 ][ 1 ], self.PairedControllers[ 2 ][ 2 ]
+		if routeCommand == "left" and controllerLeft then
+			self:TriggerPairedController( self.PairedControllers[ 1 ][ 2 ], self.PairedControllers[ 1 ][ 1 ] )
+		elseif routeCommand == "right" and controllerRight then
+			self:TriggerPairedController( self.PairedControllers[ 2 ][ 2 ], self.PairedControllers[ 2 ][ 1 ] )
+		end
+	end
+
+	self.TrackPos = self.TrackPos or Metrostroi.GetPositionOnTrack( self:GetPos() )[ 1 ] -- track location data because it doesn't work in Initialize() for some reason
+	if not IsValid( self.TargetEnt ) then
+		print( "ERROR! Target Switch not found!" )
+		return
+	end
+
+	if not self.TargetEnt.TrackPosition then
+		print( "ERROR! Target Switch Position not found!" )
+		return
+	end
+
+	if not self.PairedControllers[ 1 ] then self:GetPairedControllers() end
 	local TargetX = self.TargetEnt.TrackPosition.x -- the switch's x coordinate. Just cache it, for the gold star optimisation
 	local Target = self.TargetEnt
-	local found, dump2, TrainDetected, TrainList = UF.IsTrackOccupied(self.TrackPos.node1, self.TrackPos.x, self.TrackPos.x < TargetX, "light", TargetX) -- we only need the first and second returned values, so only take that
+	local found, dump2, TrainDetected, TrainList = UF.IsTrackOccupied( self.TrackPos.node1, self.TrackPos.x, self.TrackPos.x < TargetX, "light", TargetX ) -- we only need the first and second returned values, so only take that
 	local forward = self.TrackPos.x < self.TargetEnt.TrackPosition.x -- on the linear coordinate system, do we sit above or below the switch?
-	local controllerLeft, controllerRight = self:CheckForPairedControllers()
+	if not found then -- if no trains are in our sector, there is no point in going on
+		return
+	end
 
-	if not found then return end -- if no trains are in our sector, there is no point in going on
-	print(dump2)
 	-----------------------------------------------------------------------------
 	local train = TrainDetected -- use the first train detected as our target
-	local trainTrackPos = Metrostroi.GetPositionOnTrack(train:GetPos())[1]
-	local touchedContactFirst = (forward and trainTrackPos.x < self.TargetEnt.TrackPosition.x) or trainTrackPos.x > self.TargetEnt.TrackPosition.x
+	if not train then return end
+	local trainTrackPos = Metrostroi.GetPositionOnTrack( train:GetPos() )[ 1 ]
+	local touchedContactFirst = ( forward and trainTrackPos.x < self.TargetEnt.TrackPosition.x ) or trainTrackPos.x > self.TargetEnt.TrackPosition.x
 	if not touchedContactFirst then return end
 	if not train.IBIS then return end
 	local route = train.IBIS.Route -- the target's route, that's inportant
 	-- the line is the important bit, and always the last two digits of LineCourse, so remove those two digits. This makes it agnostic to double or triple digit line numbers, Ie line U2 being LK "0201" vs LK "90105"
-	local line = string.sub(train.IBIS.Course, 1, #train.IBIS.Course - 2)
+	local line = string.sub( train.IBIS.Course, 1, #train.IBIS.Course - 2 )
 	local override = train.IBIS.Override -- ask the IBIS if we're manually overriding the requested switch. It'll be a string of some "left" or "right".
-	IsValid(train)
 	if route and line and not override then
-		local routeCommand = UF.RoutingTable[line .. route] and UF.RoutingTable[line .. route][self.SwitchID] or nil
-		if routeCommand then Target:TriggerSwitch(UF.RoutingTable[line .. route][self.SwitchID], train) end
-		if routeCommand == "left" and controllerLeft then
-			self:TriggerPairedController(self.PairedControllers[1][2], self.PairedControllers[1][1])
-		elseif routeCommand == "right" and controllerRight then
-			self:TriggerPairedController(self.PairedControllers[2][2], self.PairedControllers[2][1])
-		end
+		runSwitchingNormal( line, route )
 	elseif override then
-		Target:TriggerSwitch(override, train)
+		Target:TriggerSwitch( override, self )
+	elseif string.lower( self.AutomaticSwitching ) ~= "none" then
+		if route and line and not override then
+			runSwitching()
+		elseif override then
+			Target:TriggerSwitch( self.AutomaticSwitching )
+			runSwitching()
+		end
 	end
 
-	local stillInRange, distance = self:CheckInRange(train)
-
+	local stillInRange, distance = self:CheckInRange( train )
 	if not stillInRange then TrainDetected = nil end
-
-	self:NextThink(CurTime() + 1.5) -- make sure this runs reliably every second.
+	self:NextThink( CurTime() + 1.5 ) -- make sure this runs reliably every second.
 end
-function ENT:CheckInRange(trainEnt)
-	local trainTrackPos = Metrostroi.GetPositionOnTrack(trainEnt:GetPos())[1] -- get the train's x coordinate
-	local forward = self.TrackPos.x < self.TargetEnt.TrackPosition.x -- on the linear coordinate system, do we sit above or below the switch?
-	local distance = (forward and trainTrackPos.x > self.TargetEnt.TrackPosition.x) and trainTrackPos.x - self.TargetEnt.TrackPosition.x or self.TargetEnt.TrackPosition.x
-				                 - trainTrackPos.x
 
+function ENT:CheckInRange( trainEnt )
+	local trainTrackPos = Metrostroi.GetPositionOnTrack( trainEnt:GetPos() )[ 1 ] -- get the train's x coordinate
+	local forward = self.TrackPos.x < self.TargetEnt.TrackPosition.x -- on the linear coordinate system, do we sit above or below the switch?
+	local distance = ( forward and trainTrackPos.x > self.TargetEnt.TrackPosition.x ) and trainTrackPos.x - self.TargetEnt.TrackPosition.x or self.TargetEnt.TrackPosition.x - trainTrackPos.x
 	return distance < 10, distance
 end
+
 function ENT:CheckForPairedControllers()
-	if self.PairedControllers[1] and not self.PairedControllers[2] then
-		return self.PairedControllers[1][1], nil
-	elseif not self.PairedControllers[1] and self.PairedControllers[2] then
-		return nil, self.PairedControllers[2][1]
-	elseif self.PairedControllers[1] and self.PairedControllers[2] then
-		return self.PairedControllers[1][1], self.PairedControllers[2][1]
+	if self.PairedControllers[ 1 ] and not self.PairedControllers[ 2 ] then
+		return self.PairedControllers[ 1 ][ 1 ], nil
+	elseif not self.PairedControllers[ 1 ] and self.PairedControllers[ 2 ] then
+		return nil, self.PairedControllers[ 2 ][ 1 ]
+	elseif self.PairedControllers[ 1 ] and self.PairedControllers[ 2 ] then
+		return self.PairedControllers[ 1 ][ 1 ], self.PairedControllers[ 2 ][ 1 ]
 	else
 		return nil, nil
 	end
 end
-function ENT:PairedControllerTrigger(dir, ent) ent:ReceivePairedCommand(dir) end
-function ENT:ReceivePairedCommand(dir) self.TargetEnt:TriggerSwitch(dir, self) end
-function ENT:KeyValue(key, value)
+
+function ENT:TriggerPairedController( dir, ent )
+	ent:ReceivePairedCommand( dir )
+end
+
+function ENT:ReceivePairedCommand( dir )
+	self.TargetEnt:TriggerSwitch( dir, self )
+end
+
+function ENT:KeyValue( key, value )
 	self.VMF = self.VMF or {}
-	self.VMF[key] = value
+	self.VMF[ key ] = value
 end
