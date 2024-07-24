009a1ab31e38c1cc04b4fd20c208c2bf3c061dbf
lillywho <lillywho@outlook.de>
2024-07-24 10:13:45 +0200

    U2_Panel: Format and rename door switches

--- lua/metrostroi/systems/sys_duewag_u2_panel.lua
+++ lua/metrostroi/systems/sys_duewag_u2_panel.lua
@@ -1,66 +1,243 @@
-Metrostroi.DefineSystem("U2_panel")
-
+Metrostroi.DefineSystem( "U2_panel" )
 function TRAIN_SYSTEM:Initialize()
+    self.Train:LoadSystem( "WarningAnnouncement", "Relay", "Switch", {
+        bass = true
+    } )
 
-    self.Train:LoadSystem("WarningAnnouncement","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Ventilation","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Deadman","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("PantoUp","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("DoorsUnlock","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("DoorsLock","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("DoorsCloseConfirm","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("DoorsSelectRight","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("DoorsSelectLeft","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Door1","Relay","Switch",{bass = true})
-    self.Train:LoadSystem("Battery","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Headlights","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("WarnBlink","Relay","Switch", {bass = true, normally_closed = false})
-    self.Train:LoadSystem("Wiper","Relay","Switch", {bass = true, normally_closed = false})
-    self.Train:LoadSystem("DriverLight","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("BatteryDisable","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("BlinkerRight","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("BlinkerLeft","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("PassengerOverground","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("PassengerUnderground","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("PantographRaise","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("PantographLower","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("ReleaseHoldingBrake","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("SetHoldingBrake","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Bell","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Horn","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("ThrowCoupler","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number1","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number2","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number3","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number4","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number5","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number6","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number7","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number8","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number9","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Number0","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Enter","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Delete","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("Destination","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("SpecialAnnouncements","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("DateAndTime","Relay","Switch", {bass = true})
-    self.Train:LoadSystem("SetPointLeft", "Relay", "Switch", {bass = true})
-    self.Train:LoadSystem("SetPointRight", "Relay", "Switch", {bass = true})
-    self.Train:LoadSystem("AnnouncerPlaying", "Relay", {bass = true})
-    self.Train:LoadSystem("Parralel", "Relay", "Switch", {bass = true, normally_closed = true})
-    self.Train:LoadSystem("Highbeam", "Relay", "Switch", {bass = true})
-    self.Train:LoadSystem("Blinker", "Relay", {bass = true})
-    self.Train:LoadSystem("ReduceBrake", "Relay", {bass=true})
-    self.Train:LoadSystem("DoorsLeft", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("DoorsRight", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button1a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button2a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button3a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button4a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button5a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button6a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button7a", "Relay", "Switch", {bass=true})
-    self.Train:LoadSystem("Button8a", "Relay", "Switch", {bass=true})
+    self.Train:LoadSystem( "Ventilation", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Deadman", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "PantoUp", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsUnlock", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsLock", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsCloseConfirm", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsSelectRight", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsSelectLeft", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Door1", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Battery", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Headlights", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "WarnBlink", "Relay", "Switch", {
+        bass = true,
+        normally_closed = false
+    } )
+
+    self.Train:LoadSystem( "Wiper", "Relay", "Switch", {
+        bass = true,
+        normally_closed = false
+    } )
+
+    self.Train:LoadSystem( "DriverLight", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "BatteryDisable", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "BlinkerRight", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "BlinkerLeft", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "PassengerOverground", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "PassengerUnderground", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "PantographRaise", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "PantographLower", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "ReleaseHoldingBrake", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "SetHoldingBrake", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Bell", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Horn", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "ThrowCoupler", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number1", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number2", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number3", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number4", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number5", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number6", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number7", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number8", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number9", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Number0", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Enter", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Delete", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Destination", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "SpecialAnnouncements", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DateAndTime", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "SetPointLeft", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "SetPointRight", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "AnnouncerPlaying", "Relay", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Parralel", "Relay", "Switch", {
+        bass = true,
+        normally_closed = true
+    } )
+
+    self.Train:LoadSystem( "Highbeam", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Blinker", "Relay", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "ReduceBrake", "Relay", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsSelectLeft", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "DoorsSelectRight", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button1a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button2a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button3a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button4a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button5a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button6a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button7a", "Relay", "Switch", {
+        bass = true
+    } )
+
+    self.Train:LoadSystem( "Button8a", "Relay", "Switch", {
+        bass = true
+    } )
 
     self.Button1a = 0
     self.Button2a = 0
@@ -70,12 +247,8 @@ function TRAIN_SYSTEM:Initialize()
     self.Button6a = 0
     self.Button7a = 0
     self.Button8a = 0
-
-
     self.Wiper = 0
-
     self.ReduceBrake = 0
-
     self.Blinker = 0
     self.Highbeam = 0
     self.BlinkerLeft = 0
@@ -116,15 +289,12 @@ function TRAIN_SYSTEM:Initialize()
     self.ThrowCoupler = 0
     self.DoorsUnlock = 0
     self.DoorCloseSignal = 0
-    
     self.Parralel = 1
     self.Headlights = 0
-    
-    self.DoorsLeft = 0
-    self.DoorsRight = 0
-
+    self.DoorsSelectLeft = 0
+    self.DoorsSelectRight = 0
 end
 
 function TRAIN_SYSTEM:Outputs()
-    return {"Wiper","BlinkerRight","BlinkerLeft","WarnBlink","Microphone","BellEngage","Horn","WarningAnnouncement", "PantographRaise", "PantographLower", "DoorsCloseConfirm", "PassengerLightsOn","PassengerLightsOff", "SetHoldingBrake", "ReleaseHoldingBrake", "DoorsCloseConfirm", "SetPointRight", "SetPointLeft", "ThrowCoupler", "Door1", "DoorsUnlock", "DoorCloseSignal", "Number1", "Number2", "Number3", "Number4", "Number6", "Number7", "Number8", "Number9", "Number0", "Destination","Delete","Route","DateAndTime","SpecialAnnouncements","Headlights"}
+    return { "Wiper", "BlinkerRight", "BlinkerLeft", "WarnBlink", "Microphone", "BellEngage", "Horn", "WarningAnnouncement", "PantographRaise", "PantographLower", "DoorsCloseConfirm", "PassengerLightsOn", "PassengerLightsOff", "SetHoldingBrake", "ReleaseHoldingBrake", "DoorsCloseConfirm", "SetPointRight", "SetPointLeft", "ThrowCoupler", "Door1", "DoorsUnlock", "DoorCloseSignal", "Number1", "Number2", "Number3", "Number4", "Number6", "Number7", "Number8", "Number9", "Number0", "Destination", "Delete", "Route", "DateAndTime", "SpecialAnnouncements", "Headlights" }
 end
