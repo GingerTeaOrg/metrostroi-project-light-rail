a6d519a9c3b20f94de2aa6553d98f6f75cb552a9
lillywho <lillywho@outlook.de>
2024-08-05 19:39:57 +0200

    UFLib: Add drawing hook for Lumino displays - this fixes drawing conflicts with HUDs and other 2D ops

--- lua/autorun/client/cl_uf.lua
+++ lua/autorun/client/cl_uf.lua
@@ -1,2 +1,10 @@
-files = file.Find("uf/cl_*.lua","LUA")
-for _,filename in pairs(files) do AddCSLuaFile("uf/"..filename) end
+files = file.Find( "uf/cl_*.lua", "LUA" )
+for _, filename in pairs( files ) do
+    AddCSLuaFile( "uf/" .. filename )
+end
+
+hook.Add( "PostDrawHUD", "RenderDFIScreens", function()
+    for _, ent in ipairs( ents.FindByClass( "gmod_track_uf_dfi" ) ) do
+        if IsValid( ent ) and ent.RenderDisplay then ent:RenderDisplay() end
+    end
+end )
