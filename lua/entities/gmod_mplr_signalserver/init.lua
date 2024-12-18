b089c2e31c9f9a673199e48bf36d7978bd4783dc
lillywho <lillywho@outlook.de>
2024-12-18 10:53:22 +0100

    Fix signalserver spawning, model setting, invisibility

--- lua/entities/gmod_mplr_signalserver/init.lua
+++ lua/entities/gmod_mplr_signalserver/init.lua
@@ -1,19 +1,21 @@
-AddCSLuaFile("shared.lua")
-include("shared.lua")
-
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" )
 function ENT:Initialize()
-    local ent = ents.FindByClass("gmod_mplr_signalserver")
+    local ent = ents.FindByClass( "gmod_mplr_signalserver" )
     if #ent > 1 then
-        SafeRemoveEntity(self)
+        SafeRemoveEntity( self )
+        return
     end
 
-    self:SetModel(self.Model or "")
-    if not self.Model then self:SetRenderMode(RENDERMODE_TRANSALPHA) self:SetColor(Color(0, 0, 0, 0)) end --if we haven't been given a proper model, just hide
+    self:SetModel( self.Model or "" )
+    if not self.Model then --if we haven't been given a proper model, just hide awoobwoobwoobwoob (V)(;,,;)(V)
+        self:SetRenderMode( RENDERMODE_TRANSALPHA )
+        self:SetColor( Color( 0, 0, 0, 0 ) )
+    end
 end
+
 function ENT:Think()
     self.BaseClass.Think()
-
-
     UF.UpdateSignalBlockOccupation()
-    self:NextThink(CurTime() + 0.25)
+    self:NextThink( CurTime() + 0.25 )
 end
