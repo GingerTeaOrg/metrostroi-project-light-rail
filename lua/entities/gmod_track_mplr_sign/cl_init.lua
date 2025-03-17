ba85a58b0165609208b8e5a9e119a01b4cab9de4
lillywho <lillywho@outlook.de>
2025-03-17 21:09:51 +0100

    MPLR Signage: Move model to clientside

--- lua/entities/gmod_track_mplr_sign/cl_init.lua
+++ lua/entities/gmod_track_mplr_sign/cl_init.lua
@@ -1,13 +1,16 @@
-include("shared.lua")
+include( "shared.lua" )
 function ENT:Initialize()
-	self.Sign = ents.CreateClientProp(self:GetNW2String("Model", self.BasePath .. "/" .. "speed_40" .. ".mdl"))
-	self.Sign:SetPos(self:GetPos())
-	self.Sign:SetAngles(self:GetAngles() - Angle(0, 90, 0))
+	self.Horizontal = self:GetNW2Int( "Horizontal", 0 )
+	self.Vertical = self:GetNW2Int( "Vertical", 0 )
+	self.Rotation = self:GetNW2Int( "Rotation", 0 )
+	self.Left = self:GetNW2Bool( "Left", false )
+	self.Sign = ents.CreateClientProp( self:GetNW2String( "Type", self.BasePath .. "/" .. "speed_40" .. ".mdl" ) )
+	local offset = self:LocalToWorld( ( self.Left and Vector( 0, -60, 0 ) or Vector( 0, 60, 0 ) ) + Vector( 0, self.Horizontal, self.Vertical ) )
+	self.Sign:SetPos( offset )
+	self.Sign:SetAngles( self:GetAngles() - Angle( 0, 90, 0 ) )
 	self.Sign:Spawn()
 end
 
-function ENT:OnRemove() 
-    if IsValid(self.Sign) then 
-        self.Sign:Remove() 
-    end 
+function ENT:OnRemove()
+	if IsValid( self.Sign ) then self.Sign:Remove() end
 end
--- lua/entities/gmod_track_mplr_sign/init.lua
+++ lua/entities/gmod_track_mplr_sign/init.lua
@@ -7,29 +7,27 @@ function ENT:KeyValue( key, value )
 end
 
 function ENT:Initialize()
-	
 	self:SetRenderMode( RENDERMODE_TRANSALPHA )
 	self:SetColor( Color( 0, 0, 0, 0 ) )
 	local pos = Metrostroi.GetPositionOnTrack( self:GetPos(), self:GetAngles() )
 	self.TrackPosition = pos[ 1 ]
 	self.Forward = self.TrackPosition.forward
 	self.Node = self.TrackPosition.node1
-	
-	self.Type = self.VMF and self.VMF.Type or "speed_40"
+	if self.VMF.Type then self:SetNW2String( "Type", self.VMF.Type ) end
+	self.Type = self.Type or self.VMF and self.VMF.Type or self:GetNW2String( "Type", "speed_40" )
 	if string.match( self.Type, "^speed", 1 ) == "speed" then
 		self.NextSign = self:FindNextSpeedSign()
 		self:SetSpeedLimitSection()
 		print( "Speedlimit" )
-	elseif string.match( self.Type, "start_of_block_operation", 1 ) == "start_of_block_operation" then
+	elseif string.match( self.Type, "^start_of_block_operation", 1 ) == "start_of_block_operation" then
 		self.NextSign = self:FindNextBlockEndSign()
 		self:OpenBlockOperation()
 	elseif string.match( self.Type, "end_of_block_operation", 1 ) == "end_of_block_operation" then
 		self.NextSign = self:FindNextBlockStartSign()
 		self:OpenBlockOperation()
+	elseif string.find( self:GetName(), "marker" ) then
+		self:StopMarker()
 	end
-
-	util.PrecacheModel( self.BasePath .. "/" .. self.Type .. ".mdl" )
-	if not self:GetNW2String( "Model" ) then self:SetNW2String( "Model", self.BasePath .. "/" .. self.Type .. ".mdl" ) end
 end
 
 function ENT:FindNextSpeedSign()
@@ -38,22 +36,24 @@ function ENT:FindNextSpeedSign()
 	local signal = UF.ScanTrackForEntity( "gmod_track_uf_signal", self.Node, self.TrackPosition.x, self.Forward, nil, true, "Type", "speed", true )
 	return sign or signal or nil
 end
+
 function ENT:FindNextBlockEndSign()
 	if not self.TrackPosition then return end
 	local sign = UF.ScanTrackForEntity( "gmod_track_mplr_sign", self.Node, self.TrackPosition.x, self.Forward, nil, true, "Type", "end_of_block_operation", true )
-	return sign or nil
+	return IsValid( sign ) and sign or nil
 end
+
 function ENT:FindNextBlockStartSign()
 	if not self.TrackPosition then return end
 	local sign = UF.ScanTrackForEntity( "gmod_track_mplr_sign", self.Node, self.TrackPosition.x, self.Forward, nil, true, "Type", "start_of_block_operation", true )
 	return sign or nil
 end
+
 function ENT:SetSpeedLimitSection()
 	local speedVar = self.Type
 	local forward = self.Forward
 	local k_v = {}
 	print( self.NextSign )
-
 	if forward and self.Type ~= "speed_clear" then
 		k_v = {
 			speed_forward = string.sub( speedVar, #speedVar - 2, #speedVar )
@@ -78,19 +78,24 @@ function ENT:SetSpeedLimitSection()
 		UF.WriteToNodeTable( self.Node, self.NextSign.Node, forward, k_v )
 	end
 end
-function ENT:RecursiveNodes( node, nextNode, k_v )
+
+function ENT:Think()
+	--print( self.Type )
+end
+
+function ENT:RecursiveNodes( node, nextNode, k_v, forward )
+	if not node then -- Safeguard for missing nodes
+		return
+	end
+
 	for k, v in pairs( k_v ) do
 		node[ k ] = v
 	end
 
 	if forward and nextNode then
-		self:RecursiveNodes( nextNode, nextNode.next )
-	elseif not nextNode then
-		for k, v in pairs( k_v ) do
-			node[ k ] = v
-		end
+		self:RecursiveNodes( nextNode, nextNode.next, k_v, forward )
 	elseif not forward and nextNode then
-		self:RecursiveNodes( nextNode, nextNode.prev )
+		self:RecursiveNodes( nextNode, nextNode.prev, k_v, forward )
 	end
 end
 
@@ -98,7 +103,13 @@ function ENT:OpenBlockOperation()
 	local k_v = {
 		indusi = true
 	}
-	if not node then return end
+
+	if not node then
+		print( "MPLRSign: Node invalid, not writing 'start of block operation'" )
+		return
+	end
+
+	print( "MPLR: Writing 'start of block operation'...", self )
 	UF.WriteToNodeTable( self.Node, self.NextSign.Node, self.Forward, k_v )
 end
 
@@ -106,6 +117,34 @@ function ENT:CloseBlockOperation()
 	local k_v = {
 		indusi = false
 	}
+
 	if not node then return end
 	UF.WriteToNodeTable( self.Node, self.NextSign.Node, self.Forward, k_v )
+end
+
+function ENT:StopMarker()
+	-- expected format for mappers: station001_platform1_h1
+	local name = string.Split( self:GetName(), "_" )
+	local station = string.sub( name[ 1 ], 1, #"station" )
+	local platform = string.sub( name[ 2 ], 1, #"platform" )
+	local myType = name[ 3 ]
+	if not tonumber( station, 10 ) then
+		print( self:GetName() .. ":", "STOP MARKER NAME INCORRECTLY FORMATTED!!" )
+		return
+	end
+
+	local stationTab = UF.StationEntsByIndex[ station ]
+	if not next( stationTab ) then
+		print( "ERROR: STATION INDEX " .. station .. "NOT FOUND!!" )
+		return
+	end
+
+	if not stationTab[ platform ] then
+		print( "ERROR: Platform Index " .. platform .. " of station" .. station .. " not found!!" )
+		return
+	end
+
+	k_v = {}
+	k_v[ myType ] = self:GetPos()
+	table.Merge( stationTab[ platform ], k_v, true )
 end
