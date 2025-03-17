6165700bcffbd2fa59120181f41ed8a3600c9ea4
lillywho <lillywho@outlook.de>
2025-03-17 21:02:46 +0100

    U2: clean up rubbish sys script

--- lua/metrostroi/systems/sys_duewag_u2b.lua
+++
@@ -1,8 +0,0 @@
-Metrostroi.DefineSystem("U2b")
-
-Metrostroi.DontAccelerateSimulation = true
-
-function TRAIN_SYSTEM:Think(dT)
-	local sectionA = self.Train.SectionA
-	self.DoorRandomness = sectionA.DoorRandomness
-end
