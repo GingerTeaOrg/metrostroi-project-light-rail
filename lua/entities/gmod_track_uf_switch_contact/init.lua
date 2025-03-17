cebf6730fd8b8685dc0b720548c1982e54e8b2b9
lillywho <lillywho@outlook.de>
2025-03-17 20:52:51 +0100

    Switching trigger: Simplify switching logic

--- lua/entities/gmod_track_uf_switch_contact/init.lua
+++ lua/entities/gmod_track_uf_switch_contact/init.lua
@@ -8,121 +8,116 @@ function ENT:Initialize()
 	self.Invisible = self.VMF.Invisible == "1" and true or false -- on the road we can just move the sign to wherever it makes sense, in tunnels we hide it
 	self:SetNW2Bool( "Invisible", self.Invisible )
 	util.PrecacheModel( "models/lilly/uf/signage/point_contact.mdl" ) -- precache for clientModel use
-	self.TrackPos = Metrostroi.GetPositionOnTrack( self:GetPos() )[ 1 ] -- where on the rail coordinates are we
-	self.PairedControllers = {} --initialise table of paired trigger points
-	self.AutomaticSwitching = self.VMF.PositionCorrespondanceOppositeDir -- what side to automatically switch to if a train approaches
-end
-
-function ENT:GetPairedControllers()
-	if self.VMF.PairedControllerLeft then
-		local controller1 = ents.FindByName( self.VMF.PairedControllerLeft[ 1 ] )
-		table.insert( self.PairedControllers, 1, { controller1, self.VMF.PairedControllerLeftCommand } )
-	end
-
-	if self.VMF.PairedControllerRight then
-		local controller2 = self.VMF.PairedControllerRight[ 1 ]
-		table.insert( self.PairedControllers, 2, { controller2, self.VMF.PairedControllerRightCommand } )
-	end
+	self.TrackPos = Metrostroi.GetPositionOnTrack( self:GetPos(), self:GetAngles() )[ 1 ] -- where on the rail coordinates are we
+	self.AutomaticSwitching = self.VMF.SecondaryAutomaticSwitching -- what side to automatically switch to if a train approaches from the other end
+	self.TrainInRange = false
+	self.TargetEnt.PairedControllers[ self ] = true
 end
 
 function ENT:Think()
-	local function runSwitchingNormal( line, route )
-		local routeCommand = UF.RoutingTable[ line .. route ] and UF.RoutingTable[ line .. route ][ self.SwitchID ] or nil
-		if next( self.PairedControllers ) then local controllerLeft, controllerRight = self.PairedControllers[ 1 ][ 1 ], self.PairedControllers[ 2 ][ 2 ] end
-		if routeCommand then Target:TriggerSwitch( UF.RoutingTable[ line .. route ][ self.SwitchID ], train ) end
-		if routeCommand == "left" and controllerLeft then
-			self:TriggerPairedController( self.PairedControllers[ 1 ][ 2 ], self.PairedControllers[ 1 ][ 1 ] )
-		elseif routeCommand == "right" and controllerRight then
-			self:TriggerPairedController( self.PairedControllers[ 2 ][ 2 ], self.PairedControllers[ 2 ][ 1 ] )
+	local function executeSwitching( line, route, isOverride, train, isAutomaticOverride )
+		local routeCommand = line and UF.RoutingTable[ line .. route ] and UF.RoutingTable[ line .. route ][ self.SwitchID ] or nil --get the routing command from the table
+		print( "exec" )
+		if isAutomaticOverride and IsValid( train ) then
+			print( "Automatic Override" )
+			self.TargetEnt:SecondarySwitchingQueue( isOverride, self )
+		elseif isOverride then
+			self.TargetEnt:SwitchingQueue( isOverride, self )
+		elseif routeCommand then
+			self.TargetEnt:SwitchingQueue( routeCommand, self )
+		else
+			print( "No switching possibility found. Quitting.", isAutomaticOverride )
+			return
 		end
 	end
 
-	local function runSwitching()
-		local routeCommand = UF.RoutingTable[ line .. route ] and UF.RoutingTable[ line .. route ][ self.SwitchID ] or nil
-		local controllerLeft, controllerRight = self.PairedControllers[ 1 ][ 1 ], self.PairedControllers[ 2 ][ 2 ]
-		if routeCommand == "left" and controllerLeft then
-			self:TriggerPairedController( self.PairedControllers[ 1 ][ 2 ], self.PairedControllers[ 1 ][ 1 ] )
-		elseif routeCommand == "right" and controllerRight then
-			self:TriggerPairedController( self.PairedControllers[ 2 ][ 2 ], self.PairedControllers[ 2 ][ 1 ] )
-		end
-	end
-
-	self.TrackPos = self.TrackPos or Metrostroi.GetPositionOnTrack( self:GetPos() )[ 1 ] -- track location data because it doesn't work in Initialize() for some reason
+	self.TrackPos = self.TrackPos or Metrostroi.GetPositionOnTrack( self:GetPos(), self:GetAngles() )[ 1 ]
 	if not IsValid( self.TargetEnt ) then
-		print( "ERROR! Target Switch not found!" )
+		print( "ERROR! Target Switch not found!", self )
+		return
+	elseif not self.TargetEnt.TrackPos then
+		print( "ERROR! Position of Target Switch on track not found!", self )
 		return
 	end
 
-	if not self.TargetEnt.TrackPosition and next( Metrostroi.Paths ) then
-		print( "ERROR! Target Switch Position not found!" )
-		return
-	elseif not self.TargetEnt.TrackPosition then
-		return
-	end
-
-	if not self.PairedControllers[ 1 ] then self:GetPairedControllers() end
-	local TargetX = self.TargetEnt.TrackPosition.x -- the switch's x coordinate. Just cache it, for the gold star optimisation
-	local Target = self.TargetEnt
-	local found, dump2, TrainDetected, TrainList = UF.IsTrackOccupied( self.TrackPos.node1, self.TrackPos.x, self.TrackPos.x < TargetX, "light", TargetX ) -- we only need the first and second returned values, so only take that
-	local forward = self.TrackPos.x < self.TargetEnt.TrackPosition.x -- on the linear coordinate system, do we sit above or below the switch?
-	if not found then -- if no trains are in our sector, there is no point in going on
-		return
-	end
-
-	-----------------------------------------------------------------------------
-	local train = TrainDetected -- use the first train detected as our target
-	if not IsValid( train ) then return end
-	local trainTrackPos = Metrostroi.GetPositionOnTrack( train:GetPos() )[ 1 ]
-	local touchedContactFirst = ( forward and trainTrackPos.x < self.TargetEnt.TrackPosition.x ) or trainTrackPos.x > self.TargetEnt.TrackPosition.x
-	if not touchedContactFirst then return end
-	if not train.IBIS then return end
-	local route = train.IBIS.Route -- the target's route, that's inportant
-	-- the line is the important bit, and always the last two digits of LineCourse, so remove those two digits. This makes it agnostic to double or triple digit line numbers, Ie line U2 being LK "0201" vs LK "90105"
-	local line = string.sub( train.IBIS.Course, 1, #train.IBIS.Course - 2 )
-	local override = train.IBIS.Override -- ask the IBIS if we're manually overriding the requested switch. It'll be a string of some "left" or "right".
-	if route and line and not override then
-		runSwitchingNormal( line, route )
-	elseif override then
-		Target:TriggerSwitch( override, self )
-	elseif string.lower( self.AutomaticSwitching ) ~= "none" then
-		if route and line and not override then
-			runSwitching()
-		elseif override then
-			Target:TriggerSwitch( self.AutomaticSwitching )
-			runSwitching()
+	local function iterateForward( node )
+		if node.next then
+			return iterateForward( node.next )
+		else
+			return node
 		end
 	end
 
-	local stillInRange, distance = self:CheckInRange( train )
-	if not stillInRange then TrainDetected = nil end
-	self:NextThink( CurTime() + 1.5 ) -- make sure this runs reliably every second.
-end
-
-function ENT:CheckInRange( trainEnt )
-	local trainTrackPos = Metrostroi.GetPositionOnTrack( trainEnt:GetPos() )[ 1 ] -- get the train's x coordinate
-	local forward = self.TrackPos.x < self.TargetEnt.TrackPosition.x -- on the linear coordinate system, do we sit above or below the switch?
-	local distance = ( forward and trainTrackPos.x > self.TargetEnt.TrackPosition.x ) and trainTrackPos.x - self.TargetEnt.TrackPosition.x or self.TargetEnt.TrackPosition.x - trainTrackPos.x
-	return distance < 10, distance
-end
-
-function ENT:CheckForPairedControllers()
-	if self.PairedControllers[ 1 ] and not self.PairedControllers[ 2 ] then
-		return self.PairedControllers[ 1 ][ 1 ], nil
-	elseif not self.PairedControllers[ 1 ] and self.PairedControllers[ 2 ] then
-		return nil, self.PairedControllers[ 2 ][ 1 ]
-	elseif self.PairedControllers[ 1 ] and self.PairedControllers[ 2 ] then
-		return self.PairedControllers[ 1 ][ 1 ], self.PairedControllers[ 2 ][ 1 ]
-	else
-		return nil, nil
+	local function iterateBackward( node )
+		if node.prev then
+			return iterateBackward( node.prev )
+		else
+			return node
+		end
 	end
-end
 
-function ENT:TriggerPairedController( dir, ent )
-	ent:ReceivePairedCommand( dir )
-end
+	local function checkInRange()
+		local inRange
+		local targetPath = self.TargetEnt.TrackPos.path.id
+		local dir = self.TrackPos.forward
+		if not self.AutomaticSwitching or self.AutomaticSwitching == "none" or targetPath == self.TrackPos.path.id then
+			inRange, _, trainDetected = UF.IsTrackOccupied( self.TrackPos.node1, self.TrackPos.x, self.TrackPos.x < self.TargetEnt.TrackPos.x, "light", self.TargetEnt.TrackPos.x )
+			return inRange, _, trainDetected
+		elseif dir then
+			local lastNode = lastNode or iterateForward( self.TrackPos.node1 )
+			print( lastNode.x, self.TrackPos.x, self.TrackPos.path.id )
+			inRange, _, trainDetected = UF.IsTrackOccupied( self.TrackPos.node1, self.TrackPos.x, dir, "light", lastNode.x )
+			return inRange, _, trainDetected
+		elseif not dir then
+			local lastNode = lastNode or iterateBackward( self.TrackPos.node1 )
+			inRange, _, trainDetected = UF.IsTrackOccupied( self.TrackPos.node1, self.TrackPos.x, dir, "light", lastNode.x )
+			return inRange, _, trainDetected
+		end
+		return false
+	end
 
-function ENT:ReceivePairedCommand( dir )
-	self.TargetEnt:TriggerSwitch( dir, self )
+	local found, _, TrainDetected = checkInRange()
+	-- Check if there are trains detected
+	if found then
+		local train = TrainDetected.IBIS and TrainDetected or TrainDetected.SectionA and TrainDetected.SectionA.IBIS and TrainDetected.SectionA or TrainDetected.SectionC and TrainDetected.SectionC.IBIS and TrainDetected.SectionC or nil
+		if not train or not train.IBIS then
+			self:NextThink( CurTime() + 1 )
+			return
+		end
+
+		local line = train.IBIS.LineLength == 2 and string.sub( train.IBIS.Course, 1, #train.IBIS.Course - 2 ) or train.IBIS.LineLength == 3 and string.sub( train.IBIS.Course, 1, #train.IBIS.Course - 2 ) or nil
+		line = line and tonumber( line, 10 ) or nil
+		local route = route and tonumber( train.IBIS.Route, 10 ) or nil
+		local override = train.IBIS.Override
+		-- Execute switching based on detected route or override
+		if route and line and not override then
+			self.TrainInRange = true -- Mark train in range
+			executeSwitching( line, route, false, train )
+			self:NextThink( CurTime() + 1 )
+			return true
+		elseif override then
+			self.TrainInRange = true -- Mark train in range
+			print( "override!" )
+			executeSwitching( line, route, override, train )
+			self:NextThink( CurTime() + 1 )
+			return true
+		elseif self.AutomaticSwitching and self.AutomaticSwitching ~= "none" then
+			print( "AutomaticOverride" )
+			self.TrainInRange = true
+			executeSwitching( nil, nil, self.AutomaticSwitching, train, true ) --line, route, isOverride, train, isAutomaticOverride
+			return true
+		end
+	else
+		-- No trains found in the range, reset state
+		if self.TrainInRange then
+			print( "Train left the range. Clearing state." )
+			self.TrainInRange = false
+			self:NextThink( CurTime() + 1 )
+			return true
+		end
+	end
+
+	self:NextThink( CurTime() + 1.0 )
 end
 
 function ENT:KeyValue( key, value )
