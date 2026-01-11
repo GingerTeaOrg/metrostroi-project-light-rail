eaa61835c26896f3aa95f66adc9840f0296bef17
lillywho <lillywho@outlook.de>
2026-01-11 17:07:45 +0100

    Fix trainequipment example file

--- lua/uf/trainequipment/example.lua
+++ lua/uf/trainequipment/example.lua
@@ -1,14 +1,39 @@
---[[
-
+-- format: multiline
 local MyBogey = {
-    u5={
-            "models/lilly/uf/u5/bogey.mdl",
-            Vector(0,0.0,0),Angle(0,0,0),nil,
-            Vector(0,-61,-14),Vector(0,61,-14),
-            nil,
-            Vector(4.3,-63,-3.3),Vector(4.3,63,-3.3),
-        },
+	u5 = {
+		"models/lilly/uf/u5/bogey.mdl",
+		Vector(
+			0, 
+			0.0, 
+			0
+		),
+		Angle(
+			0, 
+			0, 
+			0
+		),
+		nil,
+		Vector(
+			0, 
+			-61, 
+			-14
+		),
+		Vector(
+			0, 
+			61, 
+			-14
+		),
+		nil,
+		Vector(
+			4.3, 
+			-63, 
+			-3.3
+		),
+		Vector(
+			4.3, 
+			63, 
+			-3.3
+		),
+	},
 }
-
-table.Add(MPLR.BogeyTypes,MyBogey)
-]]
+--table.Merge( MPLR.BogeyTypes, MyBogey )
