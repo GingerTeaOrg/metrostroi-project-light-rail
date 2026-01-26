fad3ee3b8f3464712a49e93ea211dc29bae94c42
lillywho <lillywho@outlook.de>
2026-01-26 11:23:06 +0100

    Add file for trolleybus sim car spawn

---
+++ lua/trolleybus_system/trafficents/metrostroi.lua
@@ -0,0 +1,3 @@
+Trolleybus_System.AddEntityToAvoid( "gmod_subway_mplr_base" )
+Trolleybus_System.AddEntityToAvoid( "gmod_subway_base" )
+print( "Registered Metrostroi and Project Light Rail as vehicles to avoid while spawning AI cars." )
